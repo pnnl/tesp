c139d8889350df5f9a2be5b75a80b9e9c75a0917
temcdrm <Thomas.McDermott@pnnl.gov>
2017-04-11 08:36:33 -0700

    launching processes and collecting values

--- src/python/tesp.py
+++ src/python/tesp.py
@@ -1,21 +1,17 @@
 import sys;
 import json;
-#import matplotlib.pyplot as plt;
-#import tkinter.filedialog
 import tkinter as tk
 import tkinter.ttk as ttk
+import subprocess
+import os
+import fncs
 
 import numpy as np;
 import matplotlib;
 matplotlib.use("TkAgg");
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg;
 from matplotlib.figure import Figure;
-
-def launch_all():
-	print('launching')
-
-def kill_all():
-	print('kill all processes')
+import matplotlib.pyplot as plt;
 
 root = tk.Tk()
 root.title('Transactive Energy Simulation Platform')
@@ -49,6 +45,66 @@ vars = [['MATPOWER','GLD Bus',7,''],
 				['Houses','Base Cooling: Band',2.0,'degF'],
 				['Houses','Bid Delay',60,'s']
 				];
+
+fig, ax = plt.subplots(4,1, sharex = 'col')
+
+def launch_all():
+	print('launching all simulators')
+	subprocess.Popen ('(export FNCS_BROKER="tcp://*:5571" && exec fncs_broker 36 &> broker.log &)', shell=True)
+	subprocess.Popen ('(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf &> eplus.log &)', shell=True)
+	subprocess.Popen ('(export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m School_DualController eplus_TE_Challenge_metrics.json &> eplus_json.log &)', shell=True)
+#	subprocess.Popen ('(export FNCS_CONFIG_FILE=tracer.yaml && exec fncs_tracer 2d tracer.out &> tracer.log &)', shell=True)
+	subprocess.Popen ('(exec ./launch_TE_Challenge_agents.sh &)', shell=True)
+	subprocess.Popen ('(export FNCS_CONFIG_FILE=pypower30.yaml && export FNCS_FATAL=NO && export FNCS_LOG_STDOUT=yes && exec python fncsPYPOWER.py TE_Challenge "2013-07-01 00:00:00" 172800 300 &> pypower.log &)', shell=True)
+	print('launched all simulators')
+
+	nb.select(2)
+	root.update()
+
+	os.environ['FNCS_CONFIG_FILE'] = 'tesp.yaml'
+	os.environ['FNCS_FATAL'] = 'NO'
+	print('config file = ', os.environ['FNCS_CONFIG_FILE'])
+
+	fncs.initialize()
+	time_granted = 0
+	time_stop = 2 * 24 * 60
+	hrs=np.linspace(0.0, 48.0, 577)
+	x0 = np.zeros(577)
+	x1 = np.zeros(577)
+	x2 = np.zeros(577)
+	x3 = np.zeros(577)
+	while time_granted < time_stop:
+		time_granted = fncs.time_request(time_stop)
+		events = fncs.get_events()
+		for key in events:
+			tok = key.decode()
+			idx = int (time_granted / 60)
+			if tok == 'power_A':
+				val = 3.0 * float (fncs.get_value(key).decode().strip('+ degFkW')) / 1000.0
+#				print(hrs,tok,val)
+				x1[idx] = val
+				ax[1].plot(hrs,x1)
+			elif tok == 'house_air_temperature':
+				val = float (fncs.get_value(key).decode().strip('+ degFkW'))
+#				print(hrs,tok,val)
+				x3[idx] = val
+				ax[3].plot(hrs,x3)
+			elif tok == 'vpos7':
+				val = float (fncs.get_value(key).decode().strip('+ degFkW')) / 133000.0
+#				print(hrs,tok,val)
+				x0[idx] = val
+				ax[0].plot(hrs,x0)
+			elif tok == 'clear_price':
+				val = float (fncs.get_value(key).decode().strip('+ degFkW'))
+#				print(hrs,tok,val)
+				x2[idx] = val
+				ax[2].plot(hrs,x2)
+#			print (time_granted, key.decode(), fncs.get_value(key).decode())
+		root.update()
+	fncs.finalize()
+
+def kill_all():
+	print('kill all processes')
 
 f1 = ttk.Frame(nb, name='configuration')
 lab = ttk.Label(f1, text='Simulator', relief=tk.RIDGE)
@@ -89,9 +145,27 @@ btn = ttk.Button(f2, text='Kill All', command=kill_all)
 btn.grid(row=3, column=1, sticky=tk.NSEW)
 
 f3 = ttk.Frame(nb, name='plots')
-fig = Figure(figsize=(5,5), dpi=100)
-a = fig.add_subplot(111)
-a.plot([1,2,3,4,5,6,7,8],[5,6,1,2,6,1,3,4])
+#fig = Figure(figsize=(5,5), dpi=100)
+#a = fig.add_subplot(111)
+#a.plot([1,2,3,4,5,6,7,8],[5,6,1,2,6,1,3,4])
+#fig, ax = plt.subplots(4,1, sharex = 'col')
+
+#ax[0].plot(np.array([1,2,3,4,5,6,7,8]),np.array([5,6,1,2,6,1,3,4]), color='red')
+ax[0].set_ylabel('[pu]')
+ax[0].set_title ('PYPOWER Bus Voltage')
+
+ax[1].set_ylabel('[kW]')
+ax[1].set_title ('Primary School Load')
+
+ax[2].set_ylabel('[$]')
+ax[2].set_title ('Clearing Price')
+
+ax[3].set_ylabel('[degF]')
+ax[3].set_title ('House Temperature')
+
+ax[3].set_xlabel('Hours')
+plt.xlim(0.0,48.0)
+
 canvas = FigureCanvasTkAgg(fig, f3)
 canvas.show()
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
