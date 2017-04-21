6969b4a14f545daf038584f64b9e6fc5c342f94b
temcdrm <Thomas.McDermott@pnnl.gov>
2017-04-20 17:00:42 -0700

    tuning batch file debug levels

--- examples/loadshed/run.bat
+++ examples/loadshed/run.bat
@@ -1,7 +1,7 @@
 set FNCS_CONFIG_FILE=
 set FNCS_FATAL=NO
 set FNCS_LOG_STDOUT=yes
-set FNCS_LOG_LEVEL=DEBUG1
+set FNCS_LOG_LEVEL=DEBUG2
 start /b cmd /c fncs_broker 3 ^>broker.log 2^>^&1
 start /b cmd /c gridlabd loadshed.glm ^>gridlabd.log 2^>^&1
 start /b cmd /c fncs_player 6h loadshed.player ^>player.log 2^>^&1
