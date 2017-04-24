a12a6cdacbb7e7f5caf88509ad62c606dd007fec
temcdrm <Thomas.McDermott@pnnl.gov>
2017-04-24 07:24:43 -0700

    finished loadshed example

--- examples/loadshed/loadshed.py
+++ examples/loadshed/loadshed.py
@@ -4,6 +4,7 @@ import fncs;
 
 time_stop = int(sys.argv[1])
 time_granted = 0
+time_next = 1
 
 # requires the yaml file
 fncs.initialize()
@@ -11,7 +12,7 @@ fncs.initialize()
 print("# time key value")
 
 while time_granted < time_stop:
-	time_granted = fncs.time_request(time_stop)
+	time_granted = fncs.time_request(time_next)
 	events = fncs.get_events()
 	for key in events:
 		topic = key.decode()
@@ -19,6 +20,7 @@ while time_granted < time_stop:
 		print (time_granted, topic, value)
 		if topic == 'sw_status':
 			fncs.publish('sw_status', value)
+	time_next = time_granted + 1
 
 fncs.finalize()
 
