2c4a338008a6f510f356644f88662e26256c4eb5
temcdrm <Thomas.McDermott@pnnl.gov>
2017-04-26 14:38:33 -0400

    running te30 on Mac

--- examples/te30/run30.sh
+++ examples/te30/run30.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# (export FNCS_BROKER="tcp://*:5571" && export FNCS_LOG_LEVEL=DEBUG4 && export FNCS_LOG_STDOUT=yes && exec fncs_broker 36 &> broker.log &)
-(export FNCS_BROKER="tcp://*:5571" && exec fncs_broker 36 &> broker.log &)
-#(export FNCS_BROKER="tcp://*:5571" && exec fncs_broker 7 &> broker.log &)
+# (export FNCS_BROKER="tcp://*:5570" && export FNCS_LOG_LEVEL=DEBUG4 && export FNCS_LOG_STDOUT=yes && exec fncs_broker 36 &> broker.log &)
+(export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 36 &> broker.log &)
+#(export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 7 &> broker.log &)
 (export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf &> eplus.log &)
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m School_DualController eplus_TE_Challenge_metrics.json &> eplus_json.log &)
 (export FNCS_CONFIG_FILE=tracer.yaml && exec fncs_tracer 2d tracer.out &> tracer.log &)
