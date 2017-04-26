a7ee4f2410fca7ac62473c892cd8067570633791
temcdrm <Thomas.McDermott@pnnl.gov>
2017-04-25 18:38:29 -0700

    modified so the Python simulator finalizes normally

--- examples/loadshed/loadshed.py
+++ examples/loadshed/loadshed.py
@@ -9,15 +9,16 @@ time_next = 1
 # requires the yaml file
 fncs.initialize()
 
-print("# time key value")
+print('# time key value till', time_stop, flush=True)
 
 while time_granted < time_stop:
-	time_granted = fncs.time_request(time_next)
+	time_granted = fncs.time_request(time_stop) # time_next
+#	print('**', time_granted)
 	events = fncs.get_events()
 	for key in events:
 		topic = key.decode()
 		value = fncs.get_value(key).decode()
-		print (time_granted, topic, value)
+#		print (time_granted, topic, value, flush=True)
 		if topic == 'sw_status':
 			fncs.publish('sw_status', value)
 	time_next = time_granted + 1
--- examples/loadshed/run.bat
+++ examples/loadshed/run.bat
@@ -1,9 +1,10 @@
 set FNCS_CONFIG_FILE=
-set FNCS_FATAL=yes
+set FNCS_FATAL=no
 set FNCS_LOG_STDOUT=yes
-set FNCS_LOG_LEVEL=DEBUG2
-rem set FNCS_TRACE=yes
+set FNCS_LOG_LEVEL=DEBUG4
+set FNCS_TRACE=yes
 start /b cmd /c fncs_broker 3 ^>broker.log 2^>^&1
+set FNCS_LOG_LEVEL=
 start /b cmd /c gridlabd loadshed.glm ^>gridlabd.log 2^>^&1
 start /b cmd /c fncs_player 6h loadshed.player ^>player.log 2^>^&1
 set FNCS_CONFIG_FILE=loadshed.yaml
