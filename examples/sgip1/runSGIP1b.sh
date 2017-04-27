7265eaf623672a46f8a410fb21248582cec2dbdc
temcdrm <Thomas.McDermott@pnnl.gov>
2017-04-27 00:45:26 -0400

    correcting name from TE_Challenge to SGIP1b

--- examples/sgip1/runSGIP1b.sh
+++ examples/sgip1/runSGIP1b.sh
@@ -4,6 +4,6 @@
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m School_DualController eplus_SGIP1b_metrics.json &> eplus_json.log &)
 (export FNCS_CONFIG_FILE=tracer.yaml && exec fncs_tracer 2d tracer.out &> tracer.log &)
 (exec ./launch_SGIP1b_agents.sh &)
-(export FNCS_CONFIG_FILE=pypower.yaml && export FNCS_FATAL=NO && export FNCS_LOG_STDOUT=yes && exec python fncsPYPOWER.py TE_Challenge "2013-07-01 00:00:00" 172800 60 &> pypower.log &)
+(export FNCS_CONFIG_FILE=pypower.yaml && export FNCS_FATAL=NO && export FNCS_LOG_STDOUT=yes && exec python fncsPYPOWER.py SGIP1b "2013-07-01 00:00:00" 172800 60 &> pypower.log &)
 #(export FNCS_FATAL=NO && export FNCS_LOG_STDOUT=yes && exec ./run_fncsMATPOWER.sh SGIP1b.m real_power_demand_case9_T.txt 171800 "2013-07-01 00:00:00" SGIP1b &> matpower.log &)
 
