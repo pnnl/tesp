9e11e51eb9dcc8a7d6e99397675dc68c47adf6ad
temcdrm <Thomas.McDermott@pnnl.gov>
2018-01-14 13:36:58 -0800

    option to run case E without market

--- examples/sgip1/runSGIP1e.sh
+++ examples/sgip1/runSGIP1e.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-(export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 760 &> broker.log &)
+
+# with market
+# (export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 760 &> broker.log &)
+# (exec ./launch_SGIP1e_agents.sh &)
+
+#without market
+(export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 6 &> broker.log &)
+(export FNCS_FATAL=NO && exec gridlabd SGIP1e.glm &> gridlabd.log &)
+(export FNCS_FATAL=NO && exec python double_auction.py input/auction_registration.json SGIP1e &> auction.log &)
+(export FNCS_FATAL=NO && exec python house_controller.py input/controller_registration_house1_R1_12_47_1_tm_507_thermostat_controller.json &> control.log &)
+
+# EnergyPlus and Pypower - 3 processes
 (export FNCS_CONFIG_FILE=eplus.yaml && exec EnergyPlus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf &> eplus.log &)
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m School_DualController eplus_SGIP1e_metrics.json &> eplus_json.log &)
-(exec ./launch_SGIP1e_agents.sh &)
 (export FNCS_CONFIG_FILE=pypower.yaml && export FNCS_FATAL=NO && export FNCS_LOG_STDOUT=yes && exec python fncsPYPOWER.py SGIP1e &> pypower.log &)
 
