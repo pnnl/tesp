ac9c18f4237b559bc4e372598d68d6f52580ee80
Laurentiu <laurmarinovici@gmail.com>
2018-04-18 08:37:15 -0700

    Adding input files needed to run TE30 in Docker container from Ubuntu host.

---
+++ examples/te30/TESP-Docker-Inputs/RunningScripts/runVisualTE30ChallengeDocker.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+# clear
+SIM_ROOT="/tesp"
+# ==================================================== setting up the FNCS environment ================================================
+# FNCS broker
+fncsBroker="tcp://*:5570"
+# FNCS installation folder
+fncsDir="${SIM_ROOT}/FNCSInstall/bin"
+FNCSROOT="${SIM_ROOT}/FNCSInstall"
+echo "---------------------------------------------"
+if test -z $LD_LIBRARY_PATH
+then
+  LD_LIBRARY_PATH=.:$FNCSROOT/lib
+else
+  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$FNCSROOT/lib
+fi    
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$FNCSROOT/bin
+export LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+echo "======================================================"
+# set FNCS to print or not at the standard output
+fncsSTDOUTlog="no"
+# set FNCS to log or not the outputs for FNCS broker
+fncsFILElog="no"
+fncsLogFile="$fncsDir/fncs_broker.log" # I believe this is a default name
+# number of simulators
+# simNum=35
+# FNCS output file (if wanted)
+fncsOutFile="$fncsDir/outputFiles/fncs${simNum}Sims.out"
+# FNCS logging level
+fncsLOGlevel="DEBUG4"
+
+# =================================================== setting up the GridLAB-D environment ==================================================
+# FNCS broker for GridLAB-D
+# this need to be set in GLM file also, or is it enough in only one place?
+fncsBrGLD="tcp://localhost:5570"
+# set FNCS to print or not at standard output for GridLAB-D
+gldSTDOUTlog="no"
+# set FNCS to log or not the outputs for FNCS broker
+gldFILElog="no"
+# GridLAB-D installation folder
+gldDir="${SIM_ROOT}/GridLABD1048Install/bin"
+# GridLAB-D models array
+gldModels=("$gldDir/modelFiles/TE30_ChallengePYPOWER.glm")
+# get the number of GridLAB-D models
+gldNum=${#gldModels[@]}
+echo "There are $gldNum GridLAB-D instances."
+# GridLAB-D logging level
+gldLOGlevel="DEBUG"
+
+# ===================================================== setting up the PYPOWER environment ====================================================
+# PYPOWER installation folder
+ppDir="${SIM_ROOT}/PyPowerInstall"
+# FNCS broker setting for PYPOWER; this could also be set up in the YAML file
+fncsBrMP="tcp://localhost:5570"
+# set FNCS to print or not at standard output for PYPOWER
+ppSTDOUTlog="no"
+# set FNCS to log or not the outputs for FNCS broker
+ppFILElog="no"
+# PYPOWER configuration file for FNCS
+ppFNCSConfig="$ppDir/pypowerConfig_TE30dispload.yaml"
+# PYPOWER case
+ppCase="$ppDir/ppcasefile.py"
+# caseNum="9"
+# Total number of substations; could be different than the number running under this scripts
+gldTotalNum="1"
+# mpCase="./caseFiles/case${caseNum}_${gldTotalNum}Subst_2GenAtBus2.m"
+# real load profile file
+realLoadFile="$ppDir/inputFiles/real_power_demand_CCSI.txt"
+# reactive load profile file
+reactiveLoadFile="$ppDir/inputFiles/reactive_power_demand_CCSI.txt"
+# clearing market time/interval between OPF calculations
+ppMarketTime=300
+# stop time for MATPOWER simulation in seconds
+ppStopTime=172800
+# presumed starting time of simulation
+# needs to be double quoted when used becase the string has spaces
+ppStartTime="2012-01-01 00:00:00 PST"
+# load metrics file output
+ppLoadMetricsFile="$ppDir/metricFiles/loadBus_metrics.json"
+# dispatchable load metrics file output
+ppDispLoadMetricsFile="$ppDir/metricFiles/dispLoadBus_metrics.json"
+# generator metrics file output
+ppGeneratorMetricsFile="$ppDir/metricFiles/generatorBus_metrics.json"
+# output file
+ppOutFile="$ppDir/outputFiles/pp${gldTotalNum}Subst.out"
+# PYPOWER logging level
+ppLOGlevel="DEBUG"
+# ppLOGlevel="LMACTIME"
+scenarioName="TE30_Challenge_PYPOWER"
+
+# ====================================================== setting up the Energy Plus environment ==============================================================
+# Energy Plus installation folder
+epDir="${SIM_ROOT}/EnergyPlusInstall/Products"
+# FNCS broker setting for Energy Plus; this could also be set up in the ZPL file
+fncsBrEP="tcp://localhost:5570"
+# set FNCS to print or not at standard output for Energy Plus
+epSTDOUTlog="no"
+# set FNCS to log or not the outputs for Energy Plus
+epFILElog="no"
+# set the configuration file for Energy Plus
+epCONFfile="../eplus.yaml"
+# set Energy Plus weather file
+epWEATHERfile="../USA_AZ_Tucson.Intl.AP.722740_TMY3.epw"
+# set FNCS to log or not the outputs for FNCS broker
+epFILElog="no"
+# Energy Plus reference file
+epREFfile="../SchoolDualController.idf"
+# output file
+epLogFile="${SIM_ROOT}/EnergyPlusInstall/outputFiles/eplusTE30_Challenge_PYPOWER.out"
+
+# ======================================================== setting up the Energy Plus JSON environment ===========================================================
+# Energy Plus JSON installation folder
+epjDir="${SIM_ROOT}/EPlusJSONInstall/bin"
+# FNCS broker setting for Energy Plus JSON; this could also be set up in the ZPL file
+fncsBrEPJ="tcp://localhost:5570"
+# set FNCS to print or not at standard output for Energy Plus JSON
+epjSTDOUTlog="no"
+# set FNCS to log or not the outputs for Energy Plus JSON
+epjFILElog="no"
+# set the configuration file for Energy Plus
+epjCONFfile="../eplus_json.yaml"
+# set Energy Plus JSON stop time - mandatory parameter
+epjSTOPtime="172800s" # "2d"
+# set Energy Plus JSON aggregation time for results - mandatory parameter
+epjAGGtime="5m"
+# set Energy Plus JSON building ID
+epjBUILDid="School_DualController"
+# set Energy Plus output file
+epjOutFile="${SIM_ROOT}/EPlusJSONInstall/outputFiles/eplus_TE30_Challenge_PYPOWER_metrics.json"
+# out file
+epjLogFile="${SIM_ROOT}/EPlusJSONInstall/outputFiles/eplus_json_TE30_Challenge_PYPOWER.out"
+
+# ========================================================= setting up AGENTS configurations ========================================================================
+# TE AGENTS installation folder
+agentDir="${SIM_ROOT}/AgentsInstall"
+
+# ================================================ starting FNCS broker ===========================================================
+if test -e $fncsLogFile
+then
+  echo "$fncsLogFile exists, so I will remove it."
+  rm $fncsLogFile
+fi
+
+if test -e $fncsOutFile
+then
+  echo "$fncsOutFile exists, so I will remove it."
+  rm $fncsOutFile
+fi
+
+export FNCS_LOG_LEVEL=$fncsLOGlevel && export FNCS_BROKER=$fncsBroker && export FNCS_LOG_STDOUT=$fncsSTDOUTlog && export FNCS_LOG_FILE=$fncsFILElog && cd $fncsDir && ./fncs_broker $simNum &> $fncsOutFile &
+
+# ================================================ starting PYPOWER ==================================================================
+if test -e $ppOutFile
+then
+  echo "$ppOutFile exists, so I will remove it."
+  rm $ppOutFile
+fi
+
+export FNCS_CONFIG_FILE=$ppFNCSConfig && export FNCS_LOG_STDOUT=$ppSTDOUTlog && export FNCS_LOG_FILE=$ppFILElog && export FNCS_LOG_LEVEL=$fncsLOGlevel && export PYPOWER_LOG_LEVEL=$ppLOGlevel && cd $ppDir && python ./fncsPYPOWER.py $scenarioName &> $ppOutFile &
+
+# ================================================ starting GridLAB-D ===============================================================
+for ((i=0; i<$gldNum; i++)); do
+   # output file for each GridLAB-D instance
+   echo ${gldModels[${i}]}
+   gldOutFile="$gldDir/outputFiles/gld$((${i}+1))_${gldTotalNum}Subst.out"
+   if test -e $gldOutFile
+   then
+     echo "$gldOutFile exists, so I will remove it."
+     rm $gldOutFile
+   fi
+   export FNCS_LOG_STDOUT=$gldSTDOUTlog && export FNCS_LOG_FILE=$gldFILElog && export GLD_LOG_LEVEL=$gldLOGlevel && cd $gldDir && ./gridlabd ${gldModels[${i}]} &> $gldOutFile &
+done
+
+# ================================================ starting TE Agents ============================================================
+cd $agentDir && ./launch_TE30_Challenge_PYPOWER_agents.sh &
+
+# ================================================ starting Energy Plus ============================================================
+if test -e $epLogFile
+then
+  echo "$epLogFile exists, so I will remove it."
+  rm $epLogFile
+fi
+
+export FNCS_BROKER=$fncsBrEP && export FNCS_LOG_STDOUT=$epSTDOUTlog && export FNCS_LOG_FILE=$epFILElog && export FNCS_CONFIG_FILE=$epCONFfile && export FNCS_LOG_LEVEL=$fncsLOGLEVEL && cd $epDir && ./energyplus -w $epWEATHERfile -d output -r $epREFfile &> $epLogFile &
+
+# ================================================ starting Energy Plus JSON =======================================================
+if test -e $epjLogFile
+then
+  echo "$epjLogFile exists, so I will remove it."
+  rm $epjLogFile
+fi
+
+export FNCS_BROKER=$fncsBrEPJ && export FNCS_LOG_STDOUT=$epjSTDOUTlog && export FNCS_LOG_FILE=$epjFILElog && export FNCS_CONFIG_FILE=$epjCONFfile && export FNCS_LOG_LEVEL=$fncsLOGLEVEL && cd $epjDir && ./eplus_json $epjSTOPtime $epjAGGtime $epjBUILDid $epjOutFile  &> $epjLogFile &
+
+exit 0
---
+++ examples/te30/TESP-Docker-Inputs/runVisualTESPContainer-TE30.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+clear
+# 
+TESP_REP="laurmarinovici/tesp"
+TESP_TAG=":latest"
+TESP_CONT="tespV2"
+HOST_FOLDER="$PWD"
+
+#
+docker images
+
+# 
+if (docker inspect -f {{.State.Running}} ${TESP_CONT} &> /dev/null); then
+  echo "Container ${TESP_CONT} is already running."
+else
+  echo "===== Create container ${TESP_CONT}."
+  docker container run --name ${TESP_CONT} \
+                        -dit --env="DISPLAY" \
+                        --env="QT_X11_NO_MITSHM=1" \
+                        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+                        ${TESP_REP}${TESP_TAG}
+  export CONTAINER_ID=$(docker ps -l -q)
+  xhost +local:`docker inspect --format='{{ .Config.Hostname}}' ${CONTAINER_ID}`
+fi
+
+echo "===== List of containers on the machine."
+docker ps -a
+docker container start ${TESP_CONT}
+echo "===== Container ${TESP_CONT} has been started."
+
+# =================== TE30 Challenge Running scripts ====================================================
+echo "===== Create running scripts folder."
+docker container exec ${TESP_CONT} /bin/sh -c 'cd ${TESP} && mkdir runScripts'
+echo "===== Copy running scripts."
+docker cp ${HOST_FOLDER}/RunningScripts/tespTE30.py ${TESP_CONT}:/tesp/runScripts
+docker cp ${HOST_FOLDER}/RunningScripts/tespTE30.yaml ${TESP_CONT}:/tesp/runScripts
+docker cp ${HOST_FOLDER}/RunningScripts/fncs.py.DockerVersion ${TESP_CONT}:/tesp/runScripts/fncs.py
+docker cp ${HOST_FOLDER}/RunningScripts/runVisualTE30ChallengeDocker.sh ${TESP_CONT}:/tesp/runScripts
+docker container exec ${TESP_CONT} /bin/sh -c 'chmod u+x ${TESP}/runScripts/runVisualTE30ChallengeDocker.sh'
+
+# =================== FNCS settings =========================================================
+echo "===== Set-up FNCS paths and folders."
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${FNCS_INSTALL}/bin/playerFiles; then rmdir ${FNCS_INSTALL}/bin/playerFiles; mkdir ${FNCS_INSTALL}/bin/playerFiles; else mkdir ${FNCS_INSTALL}/bin/playerFiles; fi'
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${FNCS_INSTALL}/bin/outputFiles; then rmdir ${FNCS_INSTALL}/bin/outputFiles; mkdir ${FNCS_INSTALL}/bin/outputFiles; else mkdir ${FNCS_INSTALL}/bin/outputFiles; fi'
+# docker cp ${HOME}/work/CoSimulation/FNCSinstall/bin/playerFiles/ ${TESP_CONT}:/tesp/FNCSInstall/bin/
+# docker cp ${HOME}/work/CoSimulation/FNCSinstall/bin/outputFiles/ ${TESP_CONT}:/tesp/FNCSInstall/bin/
+
+# ================== GridLAB-D settings ============================================================
+echo "===== Set-up GridLAB-D paths and folders."
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${GLD_INSTALL}/bin/modelFiles; then rmdir ${GLD_INSTALL}/bin/modelFiles; mkdir ${GLD_INSTALL}/bin/modelFiles; else mkdir ${GLD_INSTALL}/bin/modelFiles; fi'
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${GLD_INSTALL}/bin/inputFilesTE30; then rmdir ${GLD_INSTALL}/bin/inputFilesTE30; mkdir ${GLD_INSTALL}/bin/inputFilesTE30; else mkdir ${GLD_INSTALL}/bin/inputFilesTE30; fi'
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${GLD_INSTALL}/bin/outputFiles; then rmdir ${GLD_INSTALL}/bin/outlputFiles; mkdir ${GLD_INSTALL}/bin/outputFiles; else mkdir ${GLD_INSTALL}/bin/outputFiles; fi'
+docker cp ${HOST_FOLDER}/GridLABD/modelFiles/ ${TESP_CONT}:/tesp/GridLABD1048Install/bin/
+docker cp ${HOST_FOLDER}/GridLABD/inputFilesTE30/ ${TESP_CONT}:/tesp/GridLABD1048Install/bin/
+# docker cp ${HOST_FOLDER}/GridLABD//outputFiles/ ${TESP_CONT}:/tesp/GridLABD1048Install/bin/
+
+# =================== Energy Plus settings =========================================================
+echo "===== Set-up Energy Plus paths and folders."
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${EPLUS_INSTALL}/outputFiles; then rmdir ${EPLUS_INSTALL}/outputFiles; mkdir ${EPLUS_INSTALL}/outputFiles; else mkdir ${EPLUS_INSTALL}/outputFiles; fi'
+docker cp ${HOST_FOLDER}/EnergyPlus/eplus.yaml ${TESP_CONT}:/tesp/EnergyPlusInstall/
+docker cp ${HOST_FOLDER}/EnergyPlus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw ${TESP_CONT}:/tesp/EnergyPlusInstall/
+docker cp ${HOST_FOLDER}/EnergyPlus/SchoolDualController.idf ${TESP_CONT}:/tesp/EnergyPlusInstall/
+
+# =================== Energy Plus JSON settings =========================================================
+echo "===== Set-up Energy Plus JSON paths and folders."
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${EPLUSJSON_INSTALL}/outputFiles; then rmdir ${EPLUSJSON_INSTALL}/outputFiles; mkdir ${EPLUSJSON_INSTALL}/outputFiles; else mkdir ${EPLUSJSON_INSTALL}/outputFiles; fi'
+docker cp ${HOST_FOLDER}/EnergyPlusJSON/eplus_json.yaml ${TESP_CONT}:/tesp/EPlusJSONInstall/
+
+# =================== PyPower settings =========================================================
+echo "===== Set-up PyPower paths and folders."
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${PYPOWER_INSTALL}/outputFiles; then rmdir ${PYPOWER_INSTALL}/outputFiles; mkdir ${PYPOWER_INSTALL}/outputFiles; else mkdir ${PYPOWER_INSTALL}/outputFiles; fi'
+docker cp ${HOST_FOLDER}/PyPower/NonGLDLoad.txt ${TESP_CONT}:/tesp/PyPowerInstall/
+docker cp ${HOST_FOLDER}/PyPower/ppcasefile.py ${TESP_CONT}:/tesp/PyPowerInstall/
+docker cp ${HOST_FOLDER}/PyPower/pypowerConfig_TE30dispload.yaml ${TESP_CONT}:/tesp/PyPowerInstall/
+
+# =================== Agents settings =========================================================
+echo "===== Set-up Agents paths and folders."
+docker container exec ${TESP_CONT} /bin/sh -c 'if test -e ${AGENTS_INSTALL}/inputFiles; then rmdir ${AGENTS_INSTALL}/inputFiles; mkdir ${AGENTS_INSTALL}/inputFiles; else mkdir ${AGENTS_INSTALL}/inputFiles; fi'
+docker cp ${HOST_FOLDER}/Agents/inputFilesPP ${TESP_CONT}:/tesp/AgentsInstall/inputFilesPP
+docker cp ${HOST_FOLDER}/Agents/launch_TE30_Challenge_PYPOWER_agents.sh.DockerVersion ${TESP_CONT}:/tesp/AgentsInstall/launch_TE30_Challenge_PYPOWER_agents.sh
+docker container exec ${TESP_CONT} /bin/sh -c 'chmod u+x ${AGENTS_INSTALL}/launch_TE30_Challenge_PYPOWER_agents.sh'
+
+# =================== Run TE30 =========================================================
+echo "===== Run script to simulate."
+docker container exec ${TESP_CONT} /bin/sh -c 'export TERM=xterm && echo "===== TERM = ${TERM}" && export FNCS_LOG_STDOUT=no && echo "===== FNCS_LOG_STDOUT = ${FNCS_LOG_STDOUT}" && cd /tesp/runScripts && python tespTE30.py&'
+# docker container exec ${TESP_CONT} /bin/sh -c "export TERM=xterm && cd /tesp/ && ./runTE30ChallengeDocker.sh > te30DockerRun.log &"
+# docker container exec ${TESP_CONT} /bin/sh -c "tail -f /tesp/GridLABD1048Install/bin/outputFiles/gld1_1Subst.out"
+
+# echo "===== Done with running use case. Copying the post processing scripts to the container."
+# docker cp ${HOME}/work/CoSimulation/TESP-pypower/outputFiles/process_pypower.py ${TESP_CONT}:/tesp/PyPowerInstall/outputFiles/
+# docker cp ${HOME}/work/CoSimulation/TESP-GridLABD1048install/bin/outputFiles/process_gld.py ${TESP_CONT}:/tesp/GridLABD1048Install/bin/outputFiles/
+# docker cp ${HOME}/work/CoSimulation/ENERGYPLUS_JSONinstall/outputFiles/process_eplus.py ${TESP_CONT}:/tesp/EPlusJSONInstall/outputFiles/
+# docker cp ${HOME}/work/CoSimulation/Agents/outputFiles/TE30_Challenge_PYPOWER_agent_dict.json ${TESP_CONT}:/tesp/AgentsInstall
+# docker cp ${HOME}/work/CoSimulation/Agents/outputFiles/process_agents.py ${TESP_CONT}:/tesp/AgentsInstall/
+# echo "===== Transferred post-processing files. Let's run."
+
+# USE_CASE="TE30_Challenge_PYPOWER" 
+
+echo "=========================================================================================="
+
+# echo "===== STOP ALL CONTAINERS."
+# docker stop $(docker ps -a -q)
+# echo "===== REMOVE ALL CONTAINERS."
+# docker rm $(docker ps -a -q)
+# echo "===== List of containers."
+# docker ps -a
+echo "===== Running bash in the container."
+docker container exec -it ${TESP_CONT} /bin/bash
