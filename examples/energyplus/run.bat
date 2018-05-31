5ec3855dacb2276600b4c8df918480bf01f6d126
temcdrm <Thomas.McDermott@pnnl.gov>
2018-05-31 09:55:30 -0700

    E+ example running with new support directory

--- examples/energyplus/run.bat
+++ examples/energyplus/run.bat
@@ -7,13 +7,13 @@ set FNCS_CONFIG_FILE=
 start /b cmd /c fncs_broker 4 ^>broker.log 2^>^&1
 
 set FNCS_CONFIG_FILE=eplus.yaml
-start /b cmd /c energyplus -w USA_IN_Indianapolis.Intl.AP.724380_TMY3.epw -d output -r SchoolDualController.idf ^>eplus.log 2^>^&1
+start /b cmd /c energyplus -w ..\..\support\energyplus\USA_IN_Indianapolis.Intl.AP.724380_TMY3.epw -d output -r ..\..\support\energyplus\SchoolDualController.idf ^>eplus.log 2^>^&1
 
 set FNCS_CONFIG_FILE=
 start /b cmd /c fncs_player 2d prices.txt ^>player.log 2^>^&1
 
 set FNCS_CONFIG_FILE=eplus_json.yaml
-start /b cmd /c eplus_json 2d 5m School_DualController eplus_eplus_metrics.json ^>eplus_json.log 2^>^&1
+start /b cmd /c eplus_json 2d 5m School_DualController eplus_eplus_metrics.json 0.02078 25 3 3 ^>eplus_json.log 2^>^&1
 
 set FNCS_CONFIG_FILE=tracer.yaml
 start /b cmd /c fncs_tracer 2d tracer.out ^>tracer.log 2^>^&1
