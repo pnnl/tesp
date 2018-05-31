167830f1fab8956c93b6dbf3a9e4050aeeecd187
temcdrm <Thomas.McDermott@pnnl.gov>
2018-05-31 10:51:27 -0700

    testing SGIP1 cases (startup) with support directory

--- examples/sgip1/runSGIP1c.sh
+++ examples/sgip1/runSGIP1c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 5 &> broker.log &)
-(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf &> eplus1c.log &)
+(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../../support/energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf &> eplus1c.log &)
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m School_DualController eplus_SGIP1c_metrics.json &> eplus_json1c.log &)
 (export FNCS_FATAL=NO && exec gridlabd -D USE_FNCS -D METRICS_FILE=SGIP1c_metrics.json SGIP1c.glm &> gridlabd1c.log &)
 (export FNCS_CONFIG_FILE=SGIP1b_auction.yaml && export FNCS_FATAL=NO && exec python auction.py SGIP1b_agent_dict.json SGIP1c &> auction1c.log &)
--- examples/sgip1/runSGIP1ex.bat
+++ examples/sgip1/runSGIP1ex.bat
@@ -8,7 +8,7 @@ set FNCS_CONFIG_FILE=
 start /b cmd /c fncs_broker 5 ^>broker1ex.log 2^>^&1
 
 set FNCS_CONFIG_FILE=eplus.yaml
-start /b cmd /c energyplus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf ^>eplus1ex.log 2^>^&1
+start /b cmd /c energyplus -w ../../support/energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf ^>eplus1ex.log 2^>^&1
 
 set FNCS_CONFIG_FILE=eplus_json.yaml
 start /b cmd /c eplus_json 2d 5m School_DualController eplus_SGIP1ex_metrics.json ^>eplus_json1ex.log 2^>^&1
