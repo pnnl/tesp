b29cb7d96754c47f2d33ddf9ae9d3691e1f92497
temcdrm <Thomas.McDermott@pnnl.gov>
2018-05-31 10:40:38 -0700

    testing run_new and run_new0 with support directory

--- examples/te30/run30.bat
+++ examples/te30/run30.bat
@@ -9,7 +9,7 @@ start /b cmd /c fncs_broker 35 ^>broker.log 2^>^&1
 
 rem set FNCS_LOG_LEVEL=
 set FNCS_CONFIG_FILE=eplus.yaml
-start /b cmd /c energyplus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf ^>eplus.log 2^>^&1
+start /b cmd /c energyplus -w ../../support/energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf ^>eplus.log 2^>^&1
 
 set FNCS_CONFIG_FILE=eplus_json.yaml
 start /b cmd /c eplus_json 2d 5m School_DualController eplus_TE_Challenge_metrics.json ^>eplus_json.log 2^>^&1
--- examples/te30/run_new.sh
+++ examples/te30/run_new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 5 &> broker.log &)
-(export FNCS_CONFIG_FILE=eplus.yaml && exec EnergyPlus -w ../energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../energyplus/SchoolDualController.idf &> eplus.log &)
+(export FNCS_CONFIG_FILE=eplus.yaml && exec EnergyPlus -w ../../support/energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf &> eplus.log &)
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m School_DualController eplus_TE_Challenge_metrics.json &> eplus_json.log &)
 (export FNCS_FATAL=NO && exec gridlabd -D USE_FNCS -D METRICS_FILE=TE_Challenge_metrics.json TE_Challenge.glm &> gridlabd.log &)
 (export FNCS_CONFIG_FILE=TE_Challenge_auction.yaml && export FNCS_FATAL=NO && exec python auction.py TE_Challenge_agent_dict.json TE_Challenge &> auction.log &)
