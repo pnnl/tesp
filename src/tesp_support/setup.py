e82266d608af9a9cbb9815abcfd794244b17a8c8
temcdrm <Thomas.McDermott@pnnl.gov>
2018-07-10 16:36:55 -0700

    update version number

--- src/tesp_support/setup.py
+++ src/tesp_support/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tesp_support',
-    version='0.1.1',
+    version='0.1.2',
     author='Thomas McDermott',
     author_email='Thomas.McDermott@PNNL.gov',
     description='Python support for the Transactive Energy Simulation Platform',
