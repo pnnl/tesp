e50aea429ed321bf10c361efc8fde0af327b4290
xcosmos6 <dangwh@hotmail.com>
2019-01-31 10:43:39 -0800

    forgot to uncomment lines in prepare_case.py

--- ercot/case8/prepare_case.py
+++ ercot/case8/prepare_case.py
@@ -2,14 +2,14 @@ import prep_ercot_auction as prep
 import tesp_support.glm_dict as gd
 import utilities
 
-# gd.glm_dict ('Bus1', True)
-# gd.glm_dict ('Bus2', True)
-# gd.glm_dict ('Bus3', True)
-# gd.glm_dict ('Bus4', True)
-# gd.glm_dict ('Bus5', True)
-# gd.glm_dict ('Bus6', True)
-# gd.glm_dict ('Bus7', True)
-# gd.glm_dict ('Bus8', True)
+gd.glm_dict ('Bus1', True)
+gd.glm_dict ('Bus2', True)
+gd.glm_dict ('Bus3', True)
+gd.glm_dict ('Bus4', True)
+gd.glm_dict ('Bus5', True)
+gd.glm_dict ('Bus6', True)
+gd.glm_dict ('Bus7', True)
+gd.glm_dict ('Bus8', True)
 
 utilities.write_FNCS_config_yaml_file_header()
 
