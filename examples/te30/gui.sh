18184f9e8756196eb4ecb80bd75c9b031e6e7ab1
temcdrm <Thomas.McDermott@pnnl.gov>
2019-02-05 19:13:57 -0800

    testing on Linux

--- examples/te30/gui.sh
+++ examples/te30/gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(export FNCS_CONFIG_FILE=TESP_Monitor.yaml && export FNCS_LOG_LEVEL="DEBUG2" && exec pythonw -c "import tesp_support.tesp_monitor as tesp;tesp.show_tesp_monitor()" &)
+(export FNCS_CONFIG_FILE=TESP_Monitor.yaml && exec python3 -c "import tesp_support.tesp_monitor as tesp;tesp.show_tesp_monitor()" &)
 
