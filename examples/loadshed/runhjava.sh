e01ee03d0cdd5ea823f634d93db0ccbb7023e24d
temcdrm <Thomas.McDermott@pnnl.gov>
2019-03-31 20:16:44 -0700

    switchable between FNCS and HELICS

--- examples/loadshed/runhjava.sh
+++ examples/loadshed/runhjava.sh
@@ -1,5 +1,5 @@
 javac -classpath ".:helics.jar" helicshed.java
 (exec helics_broker -f 2 --loglevel=3 --name=mainbroker &> broker.log &)
-(exec gridlabd loadshed.glm &> gridlabd.log &)
+(exec gridlabd -D WANT_HELICS=1 loadshed.glm &> gridlabd.log &)
 (exec java -classpath ".:helics.jar" -Djava.library.path="/usr/local/java" helicshed &> java.log &)
 
