501c2c4fe75c80a42eb33dc5ca5657267907c621
xcosmos6 <dangwh@hotmail.com>
2019-08-29 08:11:13 -0700

    change energyplus to EnergyPlus.

--- examples/sgip1/runSGIP1c.sh
+++ examples/sgip1/runSGIP1c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 5 &> broker.log &)
-(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../../support/energyplus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf &> eplus1c.log &)
+(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../../support/EnergyPlus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf &> eplus1c.log &)
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m SchoolDualController eplus_SGIP1c_metrics.json &> eplus_json1c.log &)
 (export FNCS_FATAL=YES && exec gridlabd -D USE_FNCS -D METRICS_FILE=SGIP1c_metrics.json SGIP1c.glm &> gridlabd1c.log &)
 (export FNCS_CONFIG_FILE=SGIP1c_substation.yaml && export FNCS_FATAL=YES && exec python3 -c "import tesp_support.api as tesp;tesp.substation_loop('SGIP1c_agent_dict.json','SGIP1c')" &> substation1c.log &)
