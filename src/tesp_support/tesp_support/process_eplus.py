d02fc7902e334ab74fb4e7b8aea3655a38c58f5c
temcdrm <Thomas.McDermott@pnnl.gov>
2020-01-08 20:17:35 -0800

    plot the total building, heating and cooling demands

--- src/tesp_support/tesp_support/process_eplus.py
+++ src/tesp_support/tesp_support/process_eplus.py
@@ -86,13 +86,13 @@ def process_eplus(nameroot):
 
     # make sure we found the metric indices of interest
     ary = lst['3600']['SchoolDualController']
-    print ("There are", len(ary), "metrics")
-    print ("1st hour price =", ary[PRICE_IDX], PRICE_UNITS)
+    print ('There are', len(ary), 'metrics')
+    print ('1st hour price =', ary[PRICE_IDX], PRICE_UNITS)
 
     # create a NumPy array of all metrics for the first building, 8760*39 doubles
     # we also want a NumPy array of times in hours
     data = np.empty(shape=(len(times),len(ary)), dtype=np.float)
-    print ("Constructed", data.shape, "NumPy array")
+    print ('Constructed', data.shape, 'NumPy array')
     i = 0
     for t in times:
         ary = lst[str(t)]['SchoolDualController']
@@ -101,43 +101,50 @@ def process_eplus(nameroot):
     hrs = np.array(times, dtype=np.float)
     denom = 3600.0
     hrs /= denom
-    print("Samples at", hrs, "hours")
 
     # display some averages
-    print ("Average price =", data[:,PRICE_IDX].mean(), PRICE_UNITS)
-    print ("Average demand =", data[:,ELECTRIC_DEMAND_IDX].mean(), ELECTRIC_DEMAND_UNITS)
-    print ("Average uncomf =", data[:,ASHRAE_HOURS_IDX].mean(), ASHRAE_HOURS_UNITS)
-    print ("Average people =", data[:,OCCUPANTS_IDX].mean(), OCCUPANTS_UNITS)
-    print ("Average cooling power =", data[:,COOLING_POWER_IDX].mean(), COOLING_POWER_UNITS)
-    print ("Average cooling temp  =", data[:,COOLING_TEMPERATURE_IDX].mean(), COOLING_TEMPERATURE_UNITS)
-    print ("Average cooling setpt =", data[:,COOLING_SETPOINT_IDX].mean(), COOLING_SETPOINT_UNITS)
-    print ("Average cooling delta =", data[:,COOLING_DELTA_IDX].mean(), COOLING_DELTA_UNITS)
-    print ("Average heating power =", data[:,HEATING_POWER_IDX].mean(), HEATING_POWER_UNITS)
-    print ("Average heating temp  =", data[:,HEATING_TEMPERATURE_IDX].mean(), HEATING_TEMPERATURE_UNITS)
-    print ("Average heating setpt =", data[:,HEATING_SETPOINT_IDX].mean(), HEATING_SETPOINT_UNITS)
-    print ("Average heating delta =", data[:,HEATING_DELTA_IDX].mean(), HEATING_DELTA_UNITS)
+    print ('Average price =', data[:,PRICE_IDX].mean(), PRICE_UNITS)
+    print ('Average demand =', data[:,ELECTRIC_DEMAND_IDX].mean(), ELECTRIC_DEMAND_UNITS)
+    print ('Average uncomf =', data[:,ASHRAE_HOURS_IDX].mean(), ASHRAE_HOURS_UNITS)
+    print ('Average people =', data[:,OCCUPANTS_IDX].mean(), OCCUPANTS_UNITS)
+    print ('Average cooling power =', data[:,COOLING_POWER_IDX].mean(), COOLING_POWER_UNITS)
+    print ('Average cooling temp  =', data[:,COOLING_TEMPERATURE_IDX].mean(), COOLING_TEMPERATURE_UNITS)
+    print ('Average cooling setpt =', data[:,COOLING_SETPOINT_IDX].mean(), COOLING_SETPOINT_UNITS)
+    print ('Average cooling delta =', data[:,COOLING_DELTA_IDX].mean(), COOLING_DELTA_UNITS)
+    print ('Average heating power =', data[:,HEATING_POWER_IDX].mean(), HEATING_POWER_UNITS)
+    print ('Average heating temp  =', data[:,HEATING_TEMPERATURE_IDX].mean(), HEATING_TEMPERATURE_UNITS)
+    print ('Average heating setpt =', data[:,HEATING_SETPOINT_IDX].mean(), HEATING_SETPOINT_UNITS)
+    print ('Average heating delta =', data[:,HEATING_DELTA_IDX].mean(), HEATING_DELTA_UNITS)
 
     # display a plot
-    fig, ax = plt.subplots(3,1, sharex = 'col')
+    fig, ax = plt.subplots(4,1, sharex = 'col')
 
-    ax[0].plot(hrs, data[:,COOLING_TEMPERATURE_IDX], color="blue", label="Actual")
-    ax[0].plot(hrs, data[:,COOLING_SETPOINT_IDX], color="red", label="Setpoint")
-    ax[0].plot(hrs, data[:,COOLING_DELTA_IDX], color="green", label="Delta")
+    ax[0].plot(hrs, data[:,COOLING_TEMPERATURE_IDX], color='blue', label='Actual')
+    ax[0].plot(hrs, data[:,COOLING_SETPOINT_IDX], color='red', label='Setpoint')
+    ax[0].plot(hrs, data[:,COOLING_DELTA_IDX], color='green', label='Delta')
     ax[0].set_ylabel(COOLING_TEMPERATURE_UNITS)
-    ax[0].set_title ("Cooling System Temperatures")
+    ax[0].set_title ('Cooling System Temperatures')
     ax[0].legend(loc='best')
 
-    ax[1].plot(hrs, data[:,HEATING_TEMPERATURE_IDX], color="blue", label="Actual")
-    ax[1].plot(hrs, data[:,HEATING_SETPOINT_IDX], color="red", label="Setpoint")
-    ax[1].plot(hrs, data[:,HEATING_DELTA_IDX], color="green", label="Delta")
+    ax[1].plot(hrs, data[:,HEATING_TEMPERATURE_IDX], color='blue', label='Actual')
+    ax[1].plot(hrs, data[:,HEATING_SETPOINT_IDX], color='red', label='Setpoint')
+    ax[1].plot(hrs, data[:,HEATING_DELTA_IDX], color='green', label='Delta')
     ax[1].set_ylabel(HEATING_TEMPERATURE_UNITS)
-    ax[1].set_title ("Heating System Temperatures")
+    ax[1].set_title ('Heating System Temperatures')
     ax[1].legend(loc='best')
 
-    ax[2].plot(hrs, data[:,PRICE_IDX], color="blue", label="Actual")
-    ax[2].set_xlabel("Hours")
+    ax[2].plot(hrs, data[:,PRICE_IDX], color='blue', label='Actual')
     ax[2].set_ylabel(PRICE_UNITS)
-    ax[2].set_title ("Real-time Price")
+    ax[2].set_title ('Real-time Price')
+
+    ax[3].plot(hrs, 0.001 * data[:,COOLING_POWER_IDX], color='blue', label='Cooling')
+    ax[3].plot(hrs, 0.001 * data[:,HEATING_POWER_IDX], color='red', label='Heating')
+    ax[3].plot(hrs, 0.001 * data[:,ELECTRIC_DEMAND_IDX], color='green', label='Total')
+    ax[3].set_ylabel('kW')
+    ax[3].set_title ('Building Demand')
+    ax[3].legend(loc='best')
+
+    ax[3].set_xlabel('Hours')
 
     plt.show()
 
