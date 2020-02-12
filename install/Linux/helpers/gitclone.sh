bd2f4192931a31b0f9a18177ea7ddc3a7b369a59
temcdrm <Thomas.McDermott@pnnl.gov>
2020-02-11 17:17:48 -0800

    helper files to set up Ubuntu with TESP_INSTALL=/opt/tesp

---
+++ install/Linux/helpers/gitclone.sh
@@ -0,0 +1,7 @@
+git clone -b develop https://github.com/FNCS/fncs.git
+git clone -b master https://github.com/GMLC-TDC/HELICS-src
+git clone -b feature/1173 https://github.com/gridlab-d/gridlab-d.git
+git clone -b fncs-v8.3.0 https://github.com/FNCS/EnergyPlus.git
+git clone -b develop https://github.com/pnnl/tesp.git
+git clone https://gitlab.com/nsnam/ns-3-dev.git
+svn export https://github.com/gridlab-d/tools/branches/klu-build-update/solver_klu/source/KLU_DLL
---
+++ install/Linux/helpers/provision.sh
@@ -0,0 +1,18 @@
+sudo apt-get -y install git
+sudo apt-get -y install build-essential
+sudo apt-get -y install autoconf
+sudo apt-get -y install libtool
+sudo apt-get -y install libjsoncpp-dev
+sudo apt-get -y install gfortran
+sudo apt-get -y install cmake
+# Java support
+sudo apt-get -y install openjdk-11-jre-headless
+sudo apt-get -y install openjdk-11-jdk-headless
+sudo ln -s /usr/lib/jvm/java-11-openjdk-amd64 /usr/lib/jvm/default-java
+# for HELICS and FNCS
+sudo apt-get -y install libzmq5-dev
+sudo apt-get -y install libczmq-dev
+# for GridLAB-D
+sudo apt-get install -y libxerces-c-dev
+sudo apt-get install -y libklu1
+sudo apt-get install -y python3-pip
