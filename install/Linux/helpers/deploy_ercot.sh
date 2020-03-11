d6b531020d2f54fdafeb84136d564841408e3234
temcdrm <Thomas.McDermott@pnnl.gov>
2020-03-11 11:22:07 -0700

    copy the input model files to /opt/tesp/share

---
+++ install/Linux/helpers/deploy_ercot.sh
@@ -0,0 +1,20 @@
+# update contents of /opt/tesp/share/ercot
+rm -rf /opt/tesp/share/ercot
+mkdir /opt/tesp/share/ercot
+cp ../../../ercot/*.* /opt/tesp/share/ercot
+
+mkdir /opt/tesp/share/ercot/buildings
+cp ../../../ercot/buildings/*.* /opt/tesp/share/ercot/buildings
+
+mkdir /opt/tesp/share/ercot/case8
+cp ../../../ercot/case8/*.* /opt/tesp/share/ercot/case8
+mkdir /opt/tesp/share/ercot/case8/dsostub
+cp ../../../ercot/case8/dsostub/*.* /opt/tesp/share/ercot/case8/dsostub
+
+mkdir /opt/tesp/share/ercot/bulk_system
+cp ../../../ercot/bulk_system/*.* /opt/tesp/share/ercot/bulk_system
+
+mkdir /opt/tesp/share/ercot/dist_system
+cp ../../../ercot/dist_system/*.* /opt/tesp/share/ercot/dist_system
+
+
---
+++ install/Linux/helpers/deploy_support.sh
@@ -0,0 +1,22 @@
+# update contents of /opt/tesp/share/support
+rm -rf /opt/tesp/share/support
+mkdir /opt/tesp/share/support
+
+mkdir /opt/tesp/share/support/energyplus
+cp ../../../support/energyplus/*.* /opt/tesp/share/support/energyplus
+
+mkdir /opt/tesp/share/support/feeders
+cp ../../../support/feeders/*.* /opt/tesp/share/support/feeders
+
+mkdir /opt/tesp/share/support/misc
+cp ../../../support/misc/*.* /opt/tesp/share/support/misc
+
+mkdir /opt/tesp/share/support/pypower
+cp ../../../support/pypower/*.* /opt/tesp/share/support/pypower
+
+mkdir /opt/tesp/share/support/schedules
+cp ../../../support/schedules/*.* /opt/tesp/share/support/schedules
+
+mkdir /opt/tesp/share/support/weather
+cp ../../../support/weather/*.* /opt/tesp/share/support/weather
+
