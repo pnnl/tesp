2fceb1ff2caae4c5eceb6500fbc625dfcea3cb68
temcdrm <Thomas.McDermott@pnnl.gov>
2020-03-11 19:41:51 -0700

    running sgip1 from /opt/tesp

--- examples/sgip1/runSGIP1e.sh
+++ examples/sgip1/runSGIP1e.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+declare -r TESP_SUPPORT=$TESP_INSTALL/share/support
+declare -r SCHED_PATH=$TESP_SUPPORT/schedules
+
 (export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 5 &> broker.log &)
 (export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w ../../support/EnergyPlus/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r ../../support/energyplus/SchoolDualController.idf &> eplus1e.log &)
 (export FNCS_CONFIG_FILE=eplus_json.yaml && exec eplus_json 2d 5m SchoolDualController eplus_SGIP1e_metrics.json &> eplus_json1e.log &)
-(export FNCS_FATAL=YES && exec gridlabd -D USE_FNCS -D METRICS_FILE=SGIP1e_metrics.json SGIP1e.glm &> gridlabd1e.log &)
+(export FNCS_FATAL=YES && exec gridlabd -D SCHED_PATH=$SCHED_PATH -D USE_FNCS -D METRICS_FILE=SGIP1e_metrics.json SGIP1e.glm &> gridlabd1e.log &)
 (export FNCS_CONFIG_FILE=SGIP1e_substation.yaml && export FNCS_FATAL=YES && exec python3 -c "import tesp_support.api as tesp;tesp.substation_loop('SGIP1e_agent_dict.json','SGIP1e')" &> substation1e.log &)
 (export FNCS_CONFIG_FILE=pypower.yaml && export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && exec python3 -c "import tesp_support.api as tesp;tesp.pypower_loop('sgip1_pp.json','SGIP1e')" &> pypower1e.log &)
 
