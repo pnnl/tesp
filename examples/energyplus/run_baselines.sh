e49407e3a35961965e49eebe1ca77259473b5239
temcdrm <Thomas.McDermott@pnnl.gov>
2020-03-20 00:14:25 -0700

    updating EnergyPlus examples to use TESP_INSTALL

---
+++ examples/energyplus/run_baselines.sh
@@ -0,0 +1,19 @@
+declare -r TESP_SUPPORT=$TESP_INSTALL/share/support/energyplus
+
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outFullServiceRestaurant -r $TESP_SUPPORT/FullServiceRestaurant.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outHospital -r $TESP_SUPPORT/Hospital.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outLargeHotel -r $TESP_SUPPORT/LargeHotel.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outLargeOffice -r $TESP_SUPPORT/LargeOffice.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outMediumOffice -r $TESP_SUPPORT/MediumOffice.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outMidriseApartment -r $TESP_SUPPORT/MidriseApartment.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outOutPatient -r $TESP_SUPPORT/OutPatient.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outPrimarySchool -r $TESP_SUPPORT/PrimarySchool.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outQuickServiceRestaurant -r $TESP_SUPPORT/QuickServiceRestaurant.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outSecondarySchool -r $TESP_SUPPORT/SecondarySchool.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outSmallHotel -r $TESP_SUPPORT/SmallHotel.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outSmallOffice -r $TESP_SUPPORT/SmallOffice.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outStandaloneRetail -r $TESP_SUPPORT/StandaloneRetail.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outStripMall -r $TESP_SUPPORT/StripMall.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outSuperMarket -r $TESP_SUPPORT/SuperMarket.idf
+energyplus -w $TESP_SUPPORT/2A_USA_TX_HOUSTON.epw -d outWarehouse -r $TESP_SUPPORT/Warehouse.idf
+
