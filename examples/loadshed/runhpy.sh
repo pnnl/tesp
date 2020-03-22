b62c9e809a47a994b691d3f51969de8611420d05
temcdrm <Thomas.McDermott@pnnl.gov>
2020-03-21 17:34:32 -0700

    working on the ns-3 example execution

--- examples/loadshed/runhpy.sh
+++ examples/loadshed/runhpy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (exec helics_broker -f 3 --loglevel=3 --name=mainbroker &> broker.log &)
-(exec gridlabd -D WANT_HELICS_NS3 loadshed.glm &> gridlabd.log &)
+(exec gridlabd --lock -D WANT_HELICS_NS3 loadshed.glm &> gridlabd.log &)
 (exec loadshedCommNetwork --helicsConfigFile=loadshedCommNetworkConfig.json --simulationRunTime=21600.0 &> network.log &)
 (exec python3 helicshed.py &> loadshed.log &)
 
