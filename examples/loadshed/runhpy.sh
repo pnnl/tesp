759e3f8ccf597066a2a411057a241da034742ce8
temcdrm <Thomas.McDermott@pnnl.gov>
2020-04-08 10:44:12 -0700

    ns-3 example works, but not with helics_recorder

--- examples/loadshed/runhpy.sh
+++ examples/loadshed/runhpy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-(exec helics_broker -f 3 --loglevel=7 --name=mainbroker &> broker.log & jobs -p > tesp.pid)
+(exec helics_broker -f 3 --loglevel=4 --name=mainbroker &> broker.log & jobs -p > tesp.pid)
 #(exec helics_recorder --input=helicsRecorder.txt --timedelta 1s --verbose --mapfile map.out --period 1s --marker 900s --stop 21600s &> recorder.log &)
 #(exec helics_recorder --input=helicsRecorder.txt --period 1s --stop 21600s &> recorder.log & jobs -p >> tesp.pid)
 #(exec helics_recorder --config-file=helicsRecorder.json --input=recorderSources.json &> recorder.log & jobs -p >> tesp.pid)
-#(exec helics_recorder --input=recorderSources.json --stop 21600s &> recorder.log & jobs -p >> tesp.pid)
+#(exec helics_recorder --input=recorderSources.json --period 60s --stop 21600s &> recorder.log & jobs -p >> tesp.pid)
 (export 'NS_LOG=loadshedCommNetwork:HelicsApplication:HelicsFilterApplication:HelicsIdTag:HelicsSimulatorImpl:HelicsStaticSinkApplication:HelicsStaticSourceApplication=level_all|prefix_func|prefix_time' && exec loadshedCommNetwork --helicsConfigFile=loadshedCommNetworkConfig.json --simulationRunTime=21600.0 &> network.log & jobs -p >> tesp.pid)
 (exec gridlabd -D NEW_ISLANDING -D WANT_HELICS_NS3 loadshed.glm &> gridlabd.log & jobs -p >> tesp.pid)
 (exec python3 helicshed.py &> loadshed.log & jobs -p >> tesp.pid)
