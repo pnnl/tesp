037df4366920f642f6ddbb67e16676f1e5eca06b
xcosmos6 <dangwh@hotmail.com>
2020-08-26 17:34:56 -0700

    add all the merged EMS building model in energyplus support folder, add energyplus helics example.

---
+++ examples/energyplus/eplusHelicsExample/run.sh
@@ -0,0 +1,5 @@
+(exec helics_broker -f 4 --loglevel=7 --name=mainbroker &> broker.log &)
+(export HELICS_CONFIG_FILE=eplus.json && exec /home/xcosmos/src/energyPlus/EnergyPlus/build/Products/energyplus -w Test.epw -d output -r Mergedh.idf &> eplus.log &)
+(exec helics_player --input=prices.txt --local --time_units=ns --stop 172800s --loglevel=7 &> player.log &)
+(exec eplus_agent_helics 172800s 300s SchoolDualController eplus_eplus_metrics.json  0.10 25 4 4 helics_eplus_agent2.json &> eplus_agent.log &)
+(exec helics_recorder --input=helicsRecorder.json --timedelta 1s --period 300s --stop 172800s --loglevel=7 &> tracer.log &)
