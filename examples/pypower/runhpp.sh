69bc7924191e4c88380e418e3c57c54f6c9316b1
temcdrm <Thomas.McDermott@pnnl.gov>
2020-09-04 14:45:41 -0700

    publish the initial load at negative time to fix the runhpy case

--- examples/pypower/runhpp.sh
+++ examples/pypower/runhpp.sh
@@ -2,6 +2,6 @@
 (exec helics_broker -f 3 --loglevel=4 --name=mainbroker &> broker.log &)
 (exec python3 -c "import tesp_support.api as tesp;tesp.pypower_loop('ppcase.json','ppcase',helicsConfig='pypowerConfig.json')" &> pypower.log &)
 (exec helics_recorder --input=helicsRecorder.txt --stop 172801s &> recorder.log &)
-(exec helics_player --input=helics_loads.txt --local --time_units s --stop 172800s &> helics_player.log &)
+(exec helics_player --input=helics_loads.txt --local --time_units=s --stop 172800s &> helics_player.log &)
 
 
