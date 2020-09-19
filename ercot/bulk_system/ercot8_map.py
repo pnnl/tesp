3b2b5f8403703a160ad0cecc5641ad621ba11115
temcdrm <Thomas.McDermott@pnnl.gov>
2020-09-18 19:23:28 -0700

    complete map legends

--- ercot/bulk_system/ercot8_map.py
+++ ercot/bulk_system/ercot8_map.py
@@ -2,51 +2,61 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import cartopy.io.shapereader as shpreader
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 import csv
 import networkx as nx
 import numpy as np
 import sys
+import zipfile
+import os
 
 featureScale = '50m'  # 10, 50 0r 110
-urbanColor = 'seagreen'
 shapePath = './Texas_SHP/'
 
-rdr1 = shpreader.Reader (shapePath + 'Tx_Census_CntyGeneralCoast_TTU.shp')
-COUNTIES = cfeature.ShapelyFeature (list(rdr1.geometries()), ccrs.PlateCarree())
-rdr2 = shpreader.Reader (shapePath + 'Tx_Census_StateGeneralCoast_TTU.shp')
-STATE = cfeature.ShapelyFeature (list(rdr2.geometries()), ccrs.PlateCarree())
-rdr3 = shpreader.Reader (shapePath + 'Tx_Interstates_General_NE.shp')
-ROADS = cfeature.ShapelyFeature (list(rdr3.geometries()), ccrs.PlateCarree())
-rdr4 = shpreader.Reader (shapePath + 'Tx_PopPlaces_TIGER.shp')
-PEOPLE = cfeature.ShapelyFeature (list(rdr4.geometries()), ccrs.PlateCarree())
-rdr5 = shpreader.Reader (shapePath + 'Tx_Rivers_General_NE.shp')
-RIVERS = cfeature.ShapelyFeature (list(rdr5.geometries()), ccrs.PlateCarree())
-rdr6 = shpreader.Reader (shapePath + 'Tx_WindTurbines_USGS.shp')
-WIND = cfeature.ShapelyFeature (list(rdr6.geometries()), ccrs.PlateCarree())
+def feature_from_archive (shp_path, zip_name, shp_root, asFeature=True):
+  zf = zipfile.ZipFile (shp_path + zip_name)
+  for ext in ['shp', 'shx', 'dbf']:
+    fname = '{:s}{:s}.{:s}'.format (shp_path, shp_root, ext)
+    if not os.path.isfile (fname):
+      zf.extract (shp_root + '.' + ext, shp_path)
+  rdr = shpreader.Reader (shp_path + shp_root)
+  if asFeature:
+    f = cfeature.ShapelyFeature (list(rdr.geometries()), ccrs.PlateCarree())
+  else:
+    f = list(rdr.geometries())
+  zf.close()
+  return f
 
-fig, ax = plt.subplots (figsize=(8, 8))
+COUNTIES = feature_from_archive (shapePath, 'Tx_Census_CntyGeneralCoast_TTU.zip', 'Tx_Census_CntyGeneralCoast_TTU')
+ROADS = feature_from_archive (shapePath, 'Tx_Interstates_General_NE.zip', 'Tx_Interstates_General_NE')
+WIND = feature_from_archive (shapePath, 'Tx_WindTurbines_USGS.zip', 'Tx_WindTurbines_USGS', False)
+URBAN = feature_from_archive (shapePath, 'TxDOT_Urbanized_Areas.zip', 'Urbanized_Area')
 
-ax.add_feature(cfeature.LAND.with_scale(featureScale), facecolor='beige') # landColor)
-ax.add_feature(cfeature.OCEAN.with_scale(featureScale), facecolor='aqua')
-#ax.add_feature(cfeature.BORDERS.with_scale(featureScale))
+plt.figure(figsize=(8, 8))
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.add_feature(cfeature.LAND.with_scale(featureScale), facecolor = 'honeydew')
 ax.add_feature(cfeature.STATES.with_scale(featureScale))
 ax.add_feature(cfeature.OCEAN.with_scale(featureScale))
 ax.add_feature(cfeature.RIVERS.with_scale(featureScale), zorder=2)
 ax.add_feature(cfeature.LAKES.with_scale(featureScale), zorder=2)
-#ax.add_feature(URBAN, facecolor=urbanColor, edgecolor=urbanColor)
 ax.add_feature(COUNTIES, facecolor='none', edgecolor='gray')
+ax.add_feature(ROADS, facecolor='none', edgecolor='magenta')
+ax.add_feature(URBAN, zorder=10, facecolor='orange', edgecolor='orange', alpha=0.3)
+ax.scatter([point.x for point in WIND], [point.y for point in WIND], 
+           transform=ccrs.PlateCarree(), s=2, c='cyan')
 ax.coastlines(featureScale)
-#ax.add_feature(ROADS)
-#ax.add_feature(STATE)
-#ax.add_feature(PEOPLE)
-#ax.add_feature(WIND, zorder=2)
 
 ax.set_extent([-107.0, -93.0, 25.0, 37.0])
+ax.gridlines (draw_labels=True)
+ax.set_xmargin (0)
+ax.set_ymargin (0)
 
 # name,bus1,bus2,kV,length[miles],#parallel,r1[Ohms/mile],x1[Ohms/mile],b1[MVAR/mile],ampacity,capacity[MW]
 dlines = np.genfromtxt('Lines8.csv', dtype=str, skip_header=1, delimiter=',') # ['U',int,int,float,float,int,float,float,float,float,float], skip_header=1, delimiter=',')
-# bus,lon,lat,load,gen,diff,caps
-dbuses = np.genfromtxt('Buses8.csv', dtype=[int, float, float, float, float, float, float], skip_header=1, delimiter=',')
+# bus,zone,lon,lat,load,gen,diff,caps
+dbuses = np.genfromtxt('Buses8.csv', dtype=[int, int, float, float, float, float, float, float], skip_header=1, delimiter=',')
 # idx,bus,mvabase,pmin,qmin,qmax,c2,c1,c0
 dunits = np.genfromtxt('Units8.csv', dtype=[int, int, float, float, float, float, float, float, float], skip_header=1, delimiter=',')
 
@@ -67,11 +77,11 @@ for e in dlines:
 		lbl345[(n1, n2)] = e[0]
 		e345.add ((n1, n2))
 		lst345.append ((n1, n2))
-		w345.append (1.5 * npar)
+		w345.append (1.25 * npar)
 
 xy = {}
 for b in dbuses:
-	xy[b[0]] = [b[1], b[2]]
+	xy[b[0]] = [b[2], b[3]]
 
 gnodes345 = nx.draw_networkx_nodes (graph, xy, nodelist=list(n345), node_color='k', node_size=60, ax=ax) # , alpha=0.3)
 glines345 = nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', width=w345, ax=ax) # , alpha=0.8)
@@ -79,9 +89,18 @@ glines345 = nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', 
 gnodes345.set_zorder(20)
 glines345.set_zorder(18)
 
-ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+lroad = mlines.Line2D ([0,1], [0,1], color='magenta')
+lurban = mpatches.Rectangle ((0, 0), 1, 1, facecolor='orange')
+lwind = mpatches.Rectangle ((0, 0), 1, 1, facecolor='cyan')
+ax.legend ([lroad, lurban, lwind, gnodes345, glines345], 
+           ['Interstate', 'Urbanized', 'WindGen', 'EHV Sub', 'EHV Line'], loc='lower left')
+
+plt.title ('Retained Buses and Lines - 345 kV')
+plt.xlabel ('Longitude [deg]')
+plt.ylabel ('Latitude [deg N]')
+plt.grid(linestyle='dotted')
 
 # Save the plot by calling plt.savefig() BEFORE plt.show()
-# plt.savefig('Ercot8.png')
+plt.savefig('Ercot8.png', pad_inches=0.1, bbox_inches = 'tight')
 
 plt.show()
--- ercot/bulk_system/ercot_map.py
+++ ercot/bulk_system/ercot_map.py
@@ -2,57 +2,56 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import cartopy.io.shapereader as shpreader
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 import csv
 import networkx as nx
 import numpy as np
 import sys
+import zipfile
+import os
 
 featureScale = '50m'  # 10, 50 0r 110
-urbanColor = 'seagreen'
-landColor = 'whitesmoke'
-#landColor = cfeature.COLORS['land']
-#print (landColor)
 shapePath = './Texas_SHP/'
 
-rdr1 = shpreader.Reader (shapePath + 'Tx_Census_CntyGeneralCoast_TTU.shp')
-COUNTIES = cfeature.ShapelyFeature (list(rdr1.geometries()), ccrs.PlateCarree())
-rdr2 = shpreader.Reader (shapePath + 'Tx_Census_StateGeneralCoast_TTU.shp')
-STATE = cfeature.ShapelyFeature (list(rdr2.geometries()), ccrs.PlateCarree())
-rdr3 = shpreader.Reader (shapePath + 'Tx_Interstates_General_NE.shp')
-ROADS = cfeature.ShapelyFeature (list(rdr3.geometries()), ccrs.PlateCarree())
-rdr4 = shpreader.Reader (shapePath + 'Tx_PopPlaces_TIGER.shp')
-PEOPLE = cfeature.ShapelyFeature (list(rdr4.geometries()), ccrs.PlateCarree())
-rdr5 = shpreader.Reader (shapePath + 'Tx_Rivers_General_NE.shp')
-RIVERS = cfeature.ShapelyFeature (list(rdr5.geometries()), ccrs.PlateCarree())
-rdr6 = shpreader.Reader (shapePath + 'Tx_WindTurbines_USGS.shp')
-WIND = cfeature.ShapelyFeature (list(rdr6.geometries()), ccrs.PlateCarree())
-rdr7 = shpreader.Reader (shapePath + 'Urbanized_Area.shp')
-URBAN = cfeature.ShapelyFeature (list(rdr7.geometries()), ccrs.PlateCarree())
+def feature_from_archive (shp_path, zip_name, shp_root, asFeature=True):
+  zf = zipfile.ZipFile (shp_path + zip_name)
+  for ext in ['shp', 'shx', 'dbf']:
+    fname = '{:s}{:s}.{:s}'.format (shp_path, shp_root, ext)
+    if not os.path.isfile (fname):
+      zf.extract (shp_root + '.' + ext, shp_path)
+  rdr = shpreader.Reader (shp_path + shp_root)
+  if asFeature:
+    f = cfeature.ShapelyFeature (list(rdr.geometries()), ccrs.PlateCarree())
+  else:
+    f = list(rdr.geometries())
+  zf.close()
+  return f
 
-#rdr1 = shpreader.Reader(shapePath + 'Texas_County_Boundaries/Texas_County_Boundaries.shp')
-#counties = list(rdr1.geometries())
-#COUNTIES = cfeature.ShapelyFeature(counties, ccrs.PlateCarree())
-
-#rdr2 = shpreader.Reader(shapePath + 'urbanap010g.shp/urbanap010g.shp')
-#urban = list(rdr2.geometries())
-#COUNTIES = cfeature.ShapelyFeature(counties, ccrs.PlateCarree())
-#URBAN = cfeature.ShapelyFeature(urban, ccrs.PlateCarree())
+COUNTIES = feature_from_archive (shapePath, 'Tx_Census_CntyGeneralCoast_TTU.zip', 'Tx_Census_CntyGeneralCoast_TTU')
+ROADS = feature_from_archive (shapePath, 'Tx_Interstates_General_NE.zip', 'Tx_Interstates_General_NE')
+WIND = feature_from_archive (shapePath, 'Tx_WindTurbines_USGS.zip', 'Tx_WindTurbines_USGS', False)
+URBAN = feature_from_archive (shapePath, 'TxDOT_Urbanized_Areas.zip', 'Urbanized_Area')
 
 plt.figure(figsize=(8, 8))
 
 ax = plt.axes(projection=ccrs.PlateCarree())
-ax.add_feature(cfeature.LAND.with_scale(featureScale), facecolor = 'mintcream') # facecolor=landColor)
+ax.add_feature(cfeature.LAND.with_scale(featureScale), facecolor = 'honeydew')
 ax.add_feature(cfeature.STATES.with_scale(featureScale))
 ax.add_feature(cfeature.OCEAN.with_scale(featureScale))
 ax.add_feature(cfeature.RIVERS.with_scale(featureScale), zorder=2)
 ax.add_feature(cfeature.LAKES.with_scale(featureScale), zorder=2)
 ax.add_feature(COUNTIES, facecolor='none', edgecolor='gray')
 ax.add_feature(ROADS, facecolor='none', edgecolor='magenta')
-ax.add_feature(WIND, zorder=10, facecolor='orange', edgecolor='orange', alpha=0.3)
-#ax.add_feature(WIND, zorder=12, facecolor='cyan', edgecolor='cyan', alpha=0.3)
+ax.add_feature(URBAN, zorder=10, facecolor='orange', edgecolor='orange', alpha=0.3)
+ax.scatter([point.x for point in WIND], [point.y for point in WIND], 
+           transform=ccrs.PlateCarree(), s=2, c='cyan')
 ax.coastlines(featureScale)
 
 ax.set_extent([-107.0, -93.0, 25.0, 37.0])
+ax.gridlines (draw_labels=True)
+ax.set_xmargin (0)
+ax.set_ymargin (0)
 
 # name,bus1,bus2,kV,length[miles],#parallel,r1[Ohms/mile],x1[Ohms/mile],b1[MVAR/mile],ampacity,capacity[MW]
 dlines = np.genfromtxt('RetainedLines.csv', dtype=str, skip_header=1, delimiter=',') # ['U',int,int,float,float,int,float,float,float,float,float], skip_header=1, delimiter=',')
@@ -88,14 +87,14 @@ for e in dlines:
 			lbl345[(n1, n2)] = e[0]
 			e345.add ((n1, n2))
 			lst345.append ((n1, n2))
-			w345.append (1.5 * npar)
+			w345.append (1.25 * npar)
 		else:
 			lbl138[(n1, n2)] = e[0]
 			n138.add (n1)
 			n138.add (n2)
 			e138.add ((n1, n2))
 			lst138.append ((n1, n2))
-			w138.append (1.0 * npar)
+			w138.append (0.8 * npar)
 
 xy = {}
 lblbus345 = {}
@@ -107,23 +106,30 @@ for b in dbuses:
 	else:
 		lblbus138[b[0]] = str(b[0]) + ':' + str(int(b[5]))
 
-#gnodes345 = nx.draw_networkx_nodes (graph, xy, nodelist=list(n345), node_color='k', node_size=60) # , alpha=0.3)
-#gnodes138 = nx.draw_networkx_nodes (graph, xy, nodelist=list(n138), node_color='b', node_size=20) # , alpha=0.3)
-#glines345 = nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', width=w345) # , alpha=0.8)
-#glines138 = nx.draw_networkx_edges (graph, xy, edgelist=lst138, edge_color='b', width=w138) # , alpha=0.8)
+gnodes345 = nx.draw_networkx_nodes (graph, xy, nodelist=list(n345), node_color='k', node_size=20) # , alpha=0.3)
+gnodes138 = nx.draw_networkx_nodes (graph, xy, nodelist=list(n138), node_color='b', node_size=10) # , alpha=0.3)
+glines345 = nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', width=w345) # , alpha=0.8)
+glines138 = nx.draw_networkx_edges (graph, xy, edgelist=lst138, edge_color='b', width=w138) # , alpha=0.8)
 
-#gnodes345.set_zorder(20)
-#gnodes138.set_zorder(19)
-#glines345.set_zorder(18)
-#glines138.set_zorder(17)
+gnodes345.set_zorder(20)
+gnodes138.set_zorder(19)
+glines345.set_zorder(18)
+glines138.set_zorder(17)
 
-# Save the plot by calling plt.savefig() BEFORE plt.show()
-#plt.savefig('Ercot200.png')
+lroad = mlines.Line2D ([0,1], [0,1], color='magenta')
+lurban = mpatches.Rectangle ((0, 0), 1, 1, facecolor='orange')
+lwind = mpatches.Rectangle ((0, 0), 1, 1, facecolor='cyan')
+ax.legend ([lroad, lurban, lwind, gnodes345, gnodes138, glines345, glines138], 
+           ['Interstate', 'Urbanized', 'WindGen', 'EHV Sub', 'HV Sub', 'EHV Line', 'HV Line'], loc='lower left')
 
-#plt.title ('Retained Buses and Lines')
+plt.title ('Retained Buses and Lines - 345 & 138 kV')
 plt.xlabel ('Longitude [deg]')
 plt.ylabel ('Latitude [deg N]')
-#plt.grid(linestyle='dotted')
+plt.grid(linestyle='dotted')
+
+# Save the plot by calling plt.savefig() BEFORE plt.show()
+plt.savefig('Ercot200.png', pad_inches=0.1, bbox_inches = 'tight')
+
 plt.show()
 
 # nx.draw_networkx_edges (graph, xy, edgelist=lst345, edge_color='r', width=1, alpha=0.3)
