6299c6962c3a304407055c0ba3607a97cf01e387
temcdrm <Thomas.McDermott@pnnl.gov>
2020-10-09 13:10:41 -0700

    working on UC test case

--- ercot/case8/dsostub/dam_loads2.m
+++ ercot/case8/dsostub/dam_loads2.m
@@ -1,6 +1,6 @@
 ## Author: tom <tom@uvm>
 ## Created: 2020-10-02
-function loadprofile = dam_loads
+function loadprofile = dam_loads2
 %% define constants
 [CT_LABEL, CT_PROB, CT_TABLE, CT_TBUS, CT_TGEN, CT_TBRCH, CT_TAREABUS, ...
     CT_TAREAGEN, CT_TAREABRCH, CT_ROW, CT_COL, CT_CHGTYPE, CT_REP, ...
@@ -14,34 +14,14 @@ loadprofile = struct( ...
     'table', CT_TLOAD, ...
     'rows', 0, ...
     'col', CT_LOAD_ALL_PQ, ...
-    'chgtype', CT_REL, ...
+    'chgtype', CT_REP, ...
     'values', [] );
 scale = 0.5;
 loadprofile.values(:, 1, 1) = [
-scale;%        0.6704;
-scale;%        0.6303;
-scale;%        0.6041;
-scale;%        0.5902;
-scale;%        0.5912;
-scale;%        0.6094;
-scale;%        0.6400;
-scale;%        0.6725;
-scale;%        0.7207;
-scale;%        0.7584;
-scale;%        0.7905;
-scale;%        0.8171;
-%scale;%        0.8428;
-%scale;%        0.8725;
-%scale;%        0.9098;
-%scale;%        0.9480;
-%scale;%        0.9831;
-%scale;%        1.0000;
-%scale;%        0.9868;
-%scale;%        0.9508;
-%scale;%        0.9306;
-%scale;%        0.8999;
-%scale;%        0.8362;
-%scale;%        0.7695;
+2370.4;
+2370.4;
+2370.4;
+2370.4;
 ];
 
 endfunction
--- ercot/case8/dsostub/solve_uc_case.m
+++ ercot/case8/dsostub/solve_uc_case.m
@@ -1,11 +1,13 @@
 clear;
 define_constants;
-mpc = loadcase ('rtmcase.m');
-mpopt = mpoption ('verbose',0,'out.all',1,'opf.dc.solver','GLPK');
-xgd = loadxgendata(dam_ucdata, mpc);
-nt = 24;
-profiles = getprofiles('dam_loads.m');
+mpc = loadcase ('rtmcase2.m');
+mpopt = mpoption('verbose', 0);
+% mpopt = mpoption ('verbose',0,'out.all',1,'opf.dc.solver','GLPK');
+xgd = loadxgendata(dam_ucdata2, mpc);
+%nt = 4; % 24;
 %profiles = [];
+profiles = getprofiles('dam_loads2.m');
+nt = size(profiles(1).values, 1); % number of periods
 mdi = loadmd(mpc, nt, xgd, [], [], profiles);
 mdo = most(mdi, mpopt);
 EPg = mdo.results.ExpectedDispatch;
