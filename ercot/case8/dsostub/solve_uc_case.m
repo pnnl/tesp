6e4548b047ea70de438d757ac378a75a804013e8
temcdrm <Thomas.McDermott@pnnl.gov>
2020-10-16 10:07:44 -0700

    runs a 4-period UC with fixed and responsive load profiles

--- ercot/case8/dsostub/solve_uc_case.m
+++ ercot/case8/dsostub/solve_uc_case.m
@@ -1,15 +1,19 @@
 clear;
 define_constants;
-mpc = loadcase ('rtmcase2.m');
-mpopt = mpoption('verbose', 0);
-% mpopt = mpoption ('verbose',0,'out.all',1,'opf.dc.solver','GLPK');
-xgd = loadxgendata(dam_ucdata2, mpc);
-%nt = 4; % 24;
-%profiles = [];
-profiles = getprofiles('dam_loads2.m');
+
+mpopt = mpoption('verbose', 0, 'out.all', 0);
+%mpopt = mpoption ('verbose',1,'out.all',1,'opf.dc.solver','GLPK');
+%mpopt = mpoption(mpopt, 'most.dc_model', 0); % use model with no network
+
+mpc = loadcase ('rtmcase3.m');
+xgd = loadxgendata(dam_ucdata3, mpc);
+profiles = getprofiles('dam_loads3.m');
+profiles = getprofiles('dam_loads4.m', profiles);
+
 nt = size(profiles(1).values, 1); % number of periods
 mdi = loadmd(mpc, nt, xgd, [], [], profiles);
+
 mdo = most(mdi, mpopt);
+ms = most_summary(mdo);
 EPg = mdo.results.ExpectedDispatch;
 Elam = mdo.results.GenPrices;
-most_summary(mdo);
