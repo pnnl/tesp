353cf1256a583ee08d069c4a006b52a44e5682cb
temcdrm <Thomas.McDermott@pnnl.gov>
2020-11-03 14:18:34 -0800

    scaling the building loads, reading BldgDef.json

--- examples/comm/test_comm.py
+++ examples/comm/test_comm.py
@@ -4,15 +4,15 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt;
 
-bldgs = {'LargeOffice':{'kW':1716.4, 'k':25.0,
-            'dT':np.array([0.0, 0.99, 1.99, 2.99, 4.99]),
-            'dP':np.array([0.0,-17.21,-26.64,-30.94,-35.31])},
-     'MidriseApartment':{'kW':54.1, 'k':20.0,
-            'dT':np.array([0.0, 0.99, 1.99, 2.99, 4.99]),
-            'dP':np.array([0.0,-5.45,-10.63,-14.69,-20.45])},
-     'StandaloneRetail':{'kW':135.5, 'k':30.0,
-            'dT':np.array([0.0, 0.99, 1.99, 2.99, 4.99]),
-            'dP':np.array([0.0,-4.31,-8.61,-12.91,-20.98])}}
+#bldgs = {'Office':{'kWScale':2.1, 'k':25.0,
+#            'dT':[0.0, 0.99, 1.99, 2.99, 4.99],
+#            'dP':[0.0,-17.21,-26.64,-30.94,-35.31]},
+#     'Apartments':{'kWScale':50.0, 'k':20.0,
+#            'dT':[0.0, 0.99, 1.99, 2.99, 4.99],
+#            'dP':[0.0,-5.45,-10.63,-14.69,-20.45]},
+#     'Shopping':{'kWScale':31.25, 'k':30.0,
+#            'dT':[0.0, 0.99, 1.99, 2.99, 4.99],
+#            'dP':[0.0,-4.31,-8.61,-12.91,-20.98]}}
 
 def load_bid (dT, dP, k, scale):
   bid = {'q': scale * np.array (dP), 'p': k * np.array(dT)}
@@ -25,9 +25,24 @@ if __name__ == '__main__':
   print ('usage: python3 test_comm.py 50.0 10.0   plots the reponse to variable supply offers 50 +/- 10')
   print ('       python3 test_comm.py 70          tabulates the reponse to one supply offer of 70')
   print ('       python3 test_comm.py             tabulates the reponse to one default supply offer of 60')
+  print ('       reads building load and bidding definitions from BldgDef.json')
+
+  fp = open ('BldgDef.json', 'r')
+  bldgs = json.load (fp)
+  fp.close()
+
   print ('Summary of Buildings:')
   for bldg, row in bldgs.items():
     print ('  ', bldg, row)
+
+# fp = open ('BldgDef.json', 'w')
+# json.dump (bldgs, fp, indent=2)
+# fp.close()
+
+  # make python arrays into Numpy arrays
+  for bldg, row in bldgs.items():
+    row['dT'] = np.array (row['dT'])
+    row['dP'] = np.array (row['dP'])
 
   # build the composite load bid curve
   pset = set()
@@ -35,7 +50,7 @@ if __name__ == '__main__':
 
   print ('Building Bid Curves:')
   for key, row in bldgs.items():
-    bid = load_bid (row['dT'], row['dP'], row['k'], 1.0)
+    bid = load_bid (row['dT'], row['dP'], row['k'], row['kWScale'])
     bids[key] = bid
     print ('  {:20s} quantity = '.format (key), bid['q'])
     print ('  {:20s}  price = '.format (' '), bid['p'])
@@ -76,7 +91,7 @@ if __name__ == '__main__':
         total[i] += qval
         bldg_plot[key]['qcleared'][i] = qval
         bldg = bldgs[key]
-        dTemp = np.interp (qval, -bldg['dP'], bldg['dT'])
+        dTemp = np.interp (qval, -bldg['dP']*bldg['kWScale'], bldg['dT'])
         bldg_plot[key]['dTemp'][i] = dTemp
 
     fig, ax = plt.subplots(1, 3, figsize=(18,8))
@@ -123,7 +138,7 @@ if __name__ == '__main__':
     for key, row in bids.items():
       qval = abs (np.interp (pclear, row['p'], row['q']))
       bldg = bldgs[key]
-      dTemp = np.interp (qval, -bldg['dP'], bldg['dT'])
+      dTemp = np.interp (qval, -bldg['dP']*bldg['kWScale'], bldg['dT'])
       qtotal += qval
       print ('  {:20s} {:8.2f} {:7.2f}'.format (key, qval, dTemp))
 
