96b0422fb28bc54ecff181c05432b973f750adf2
temcdrm <Thomas.McDermott@pnnl.gov>
2020-12-11 14:47:52 -0800

    don't set JAVAPATH globally

--- install/Linux/helpers/tesp_envar.sh
+++ install/Linux/helpers/tesp_envar.sh
@@ -2,6 +2,5 @@ export TESP_INSTALL="/opt/tesp"
 export PATH="$PATH:$TESP_INSTALL:$TESP_INSTALL/bin:$TESP_INSTALL/PreProcess:$TESP_INSTALL/PostProcess"
 export GLPATH="$TESP_INSTALL/lib/gridlabd:$TESP_INSTALL/share/gridlabd"
 export CXXFLAGS="-I$TESP_INSTALL/share/gridlabd"
-export JAVAPATH="$TESP_INSTALL/java:$JAVAPATH"
 
 
