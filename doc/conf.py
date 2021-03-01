dd2cdac6a6900655fb67f878ca8bdf4d073c37e7
Trevor Hardy <trevor.hardy@pnnl.gov>
2021-03-01 13:29:10 -0800

    Test inline math rendering 5

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '3.2.1'
+needs_sphinx = '3.5.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
