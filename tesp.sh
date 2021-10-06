a8f909f9b8efb761fac223d93229486e52ebd94e
d3j331 <mitch.pelton@pnnl.gov>
2021-10-06 16:53:18 -0400

    clone and build scripts

--- tesp.sh
+++ tesp.sh
@@ -15,6 +15,9 @@ sudo apt-get -y install libjsoncpp-dev
 sudo apt-get -y install gfortran
 sudo apt-get -y install cmake
 sudo apt-get -y install subversion
+sudo apt-get -y install wget
+sudo apt-get -y install pkg-config
+sudo apt-get -y install unzip
 
 # Java support
 sudo apt-get -y install openjdk-11-jre-headless
@@ -24,6 +27,7 @@ sudo ln -s /usr/lib/jvm/java-11-openjdk-amd64 /usr/lib/jvm/default-java
 # for HELICS and FNCS
 sudo apt-get -y install libzmq5-dev
 sudo apt-get -y install libczmq-dev
+sudo apt-get -y install libboost-dev
 
 # for GridLAB-D
 sudo apt-get -y install libxerces-c-dev
@@ -31,9 +35,11 @@ sudo apt-get -y install libhdf5-serial-dev
 sudo apt-get -y install libsuitesparse-dev
 # end users replace libsuitesparse-dev with libklu1, which is licensed LGPL
 
-# for AMES market simulator
+# for solvers Ipopt/cbc used by AMES/Agents
 sudo apt-get -y install coinor-cbc
-#need ipopt support
+sudo apt-apt -y install coinor-libipopt-dev
+sudo apt-get -y install liblapack-dev
+sudo apt-get -y install libmetis-dev
 
 # Python support
 # if not using miniconda (avoid Python 3.7 on Ubuntu for now)
@@ -41,57 +47,68 @@ sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-tk
 
 #              coinor-libcbc-dev \
-##              default-jdk \
-##              default-jre \
 #              gosu \
 ##              libboost-dev \
 ##              libboost-filesystem-dev \
 ##              libboost-program-options-dev \
 ##              libboost-signals-dev \
 ##              libboost-test-dev \
-#              libmetis-dev \
-#              libxerces-c-dev \
 #              lsof \
 #              make \
-#              pkg-config \
 #              python-minimal \
 #              python-pip \
 #              python3 \
 #              python3-dev \
 ##              swig \
-#              unzip \
 #              uuid-dev \
-#              wget \
-
-# Install all pip libraries
-pip install wheel colorama glm seaborn matplotlib networkx==2.3 numpy pandas pulp pyutilib==5.8.0 pyomo==5.6.8 PYPOWER scikit-learn scipy tables h5py xlrd
 
 # Set create directory structure for grid repository and installed software
 mkdir grid
-cd grid
+cd grid || exit
 mkdir repository
 mkdir installed
 mkdir software
-
 # Download all relevant repositories
-cd repository
+cd repository || exit
 # Set your name and email address
 #git config --global user.name "your user name"
 #git config --global user.email "your email"
 #git config --global credential.helper store
+
+# FNCS
 #develop for dsot
 git clone -b develop https://github.com/FNCS/fncs.git
 #feature/opendss for tesp
 #git clone -b feature/opendss https://github.com/FNCS/fncs.git
-git clone -b main https://github.com/GMLC-TDC/HELICS-src
-# develop - dec21 commit number for dsot
-# ENV GLD_VERSION=6c983d8daae8c6116f5fd4d4ccb7cfada5f8c9fc
+
+# HELICS
+git clone -b helics2 https://github.com/GMLC-TDC/HELICS-src
+#git clone -b main https://github.com/GMLC-TDC/HELICS-src
+
+# GRIDLAB
+#develop - dec21 commit number for dsot
+#ENV GLD_VERSION=6c983d8daae8c6116f5fd4d4ccb7cfada5f8c9fc
 git clone -b develop https://github.com/gridlab-d/gridlab-d.git
+
+# ENERGYPLUS
 git clone -b fncs_9.3.0 https://github.com/FNCS/EnergyPlus.git
+
+# TESP
+git clone -b evolve https://github.com/pnnl/tesp.git
+# need for back port of DSOT
 git clone -b master https://stash.pnnl.gov/scm/tesp/tesp-private.git
-git clone -b develop https://github.com/pnnl/tesp.git
+
+# NS3
 git clone https://gitlab.com/nsnam/ns-3-dev.git
-#git clone https://github.com/ames-market/psst.git
+cd ns-3-dev || exit
+git clone -b feature/13b https://github.com/GMLC-TDC/helics-ns3 contrib/helics
+cd ..
+git clone https://github.com/gjcarneiro/pybindgen.git
+
+# PSST
+git clone https://github.com/ames-market/psst.git
+
+# KLU SOLVER
 svn export https://github.com/gridlab-d/tools/branches/klu-build-update/solver_klu/source/KLU_DLL
 
 # Install snap Pycharm IDE for python
@@ -101,4 +118,4 @@ svn export https://github.com/gridlab-d/tools/branches/klu-build-update/solver_k
 # pycharm-community &> ~/charm.log&
 
 # Compile all relevant executables
-./tespCompile.sh
+./tesp/tespCompile.sh
--- tespCompile.sh
+++ tespCompile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 clear
-JP=-j4
 
 #Compile exports
 export WAREDIR=$HOME/grid/software
@@ -43,19 +42,23 @@ export PATH=$PATH:$JAVA_HOME
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTDIR/lib
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/osboxes/grid/repository/fncs/java/build
 
+# Install all pip libraries
+pip3 install wheel colorama glm seaborn matplotlib networkx==2.3 numpy pandas pulp pyutilib==5.8.0 pyomo==5.6.8 PYPOWER scikit-learn scipy tables h5py xlrd
+
+
 echo #++++++++++++++ FNCS
-cd $REPODIR/fncs
+cd "${REPODIR}/fncs" || exit
 autoreconf -isf
-./configure 'CXXFLAGS=-w -O2' 'CFLAGS=-w -O2' --prefix=$INSTDIR
+./configure 'CXXFLAGS=-w -O2' 'CFLAGS=-w -O2' --prefix="${INSTDIR}"
 # leave off --prefix if using the /usr/local
-make $JP
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
 sudo make install
 
 echo #++++++++++++++ FNCS Java lib
-cd java
+cd java || exit
 rm -r build
 mkdir build
-cd build
+cd build || exit
 
 #fix include and lib path in cmakelist.txt lines 11, 12
 #-FIND_PATH(FNCS_INCLUDE_DIR fncs.hpp)
@@ -78,71 +81,138 @@ cd build
 #)
 
 cmake ..
-make -j4
-cp fncs.jar $HOME/grid/repository/tesp/examples/loadshed/java
-cp libJNIfncs.so $HOME/grid/repository/tesp/examples/loadshed/java
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
+mkdir "${TESPDIR}/examples/capabilities/loadshed/java"
+cp fncs.jar "${TESPDIR}/examples/capabilities/loadshed/java/"
+cp libJNIfncs.so "${TESPDIR}/examples/capabilities/loadshed/java/"
 
 
 echo #++++++++++++++ HELICS
-cd $REPODIR/HELICS-src
+cd "${REPODIR}/HELICS-src" || exit
 sudo rm -r build
 mkdir build
-cd build
+cd build || exit
 cmake -DBUILD_JAVA_INTERFACE=ON -DBUILD_SHARED_LIBS=ON \
       -DJAVA_AWT_INCLUDE_PATH=NotNeeded -DHELICS_DISABLE_BOOST=ON \
-      -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_BUILD_TYPE=Release ..
+      -DCMAKE_INSTALL_PREFIX="${INSTDIR}" -DCMAKE_BUILD_TYPE=Release ..
 # leave off -DCMAKE_INSTALL_PREFIX if using the default /usr/local
 git submodule update --init
-make $JP
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
 sudo make install
 
 
 echo #++++++++++++++ KLU solver
-cd $REPODIR/KLU_DLL
+cd "${REPODIR}/KLU_DLL" || exit
 sudo rm -r build
 mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTDIR ..
+cd build || exit
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${INSTDIR}" ..
 # replace $INSTDIR with /usr/local if using the default
 sudo cmake --build . --target install
 
 
 echo #++++++++++++++ GridLAB-D
-cd "$REPODIR/gridlab-d"
+cd "${REPODIR}/gridlab-d" || exit
 autoreconf -isf
 # for ARM/'constance' processer needs libtinfo.a,   edit 'ax_with_curses.m4'  lines 206,219,325,338  add -ltinfo
-./configure --prefix=$INSTDIR --with-fncs=$INSTDIR --with-hdf5=yes --enable-silent-rules 'CFLAGS=-w -O2' 'CXXFLAGS=-w -O2 -std=c++14' 'LDFLAGS=-w'
+./configure --prefix="${INSTDIR}" --with-fncs="${INSTDIR}" --with-hdf5=yes --enable-silent-rules \
+            'CFLAGS=-w -O2' 'CXXFLAGS=-w -O2 -std=c++14' 'LDFLAGS=-w'
 # in configure --with-fncs and --with-helics can not be left blank, so use either $INSTDIR or /usr/local for both
 # leave off --prefix if using the default /usr/local
 # for debugging use 'CXXFLAGS=-w -g -O0' and 'CFLAGS=-w -std=c++14 -g -O0' and 'LDFLAGS=-w -g -O0'
-
-make $JP
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
 sudo make install
 #gridlabd --validate
 
 
-#++++++++++++++  EngeryPlus
-cd $INSTDIR
+#++++++++++++++  EnergyPlus
+cd "${INSTDIR}" || exit
 mkdir energyplus
-cd $REPODIR/EnergyPlus
+cd "${REPODIR}/EnergyPlus" || exit
 sudo rm -r build
 mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$INSTDIR/energyplus -DCMAKE_PREFIX_PATH=$INSTDIR -DBUILD_FORTRAN=ON -DBUILD_PACKAGE=ON -DENABLE_INSTALL_REMOTE=OFF ..
+cd build || exit
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${INSTDIR}/energyplus" -DCMAKE_PREFIX_PATH="${INSTDIR}" -DBUILD_FORTRAN=ON -DBUILD_PACKAGE=ON -DENABLE_INSTALL_REMOTE=OFF ..
 # todo warning[-Wmissing-include-dirs]
 # leave off -DCMAKE_INSTALL_PREFIX if using the default /usr/local
-make $JP
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
 sudo make install
 
 
-#++++++++++++++  EngeryPlus Json
-cd $TESPDIR/src/energyplus
+#++++++++++++++  EnergyPlus Json
+cd "${TESPDIR}/src/energyplus" || exit
 # the following steps are also in go.sh
 autoheader
 aclocal
 automake --add-missing
 autoconf
-./configure --prefix=$INSTDIR  --with-fncs=$INSTDIR 'CXXFLAGS=-w -O2' 'CFLAGS=-w -O2'
-make $JP
+./configure --prefix="${INSTDIR}"  --with-fncs="${INSTDIR}" 'CXXFLAGS=-w -O2' 'CFLAGS=-w -O2'
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
 sudo make install
 
+#++++++++++++++ NS-3
+cd "${REPODIR}/pybindgen"
+sudo python3 setup.py install
+
+cd "${REPODIR}/ns-3-dev"
+# first build: use the following command for HELICS interface to ns3:
+# git clone -b feature/13b https://github.com/GMLC-TDC/helics-ns3 contrib/helics
+# subsequent builds: use the following 3 commands to update HELICS interface code:
+# cd contrib/helics
+# git pull
+# cd ../..
+# then configure, build and test ns3 with the HELICS interface
+# --with-helics may not be left blank, so use either $TESP_INSTALL or /usr/local
+./waf distclean
+./waf configure --prefix="${INSTDIR}" --with-helics="${INSTDIR}" --build-profile=optimized --disable-werror --enable-logs --enable-examples --enable-tests
+./waf build
+sudo ./waf install
+./test.py
+
+
+#++++++++++++++ Ipopt
+IPOPT_VERSION=3.13.2
+cd "${WAREDIR}" || exit
+wget https://www.coin-or.org/download/source/Ipopt/Ipopt-${IPOPT_VERSION}.tgz
+tar -xzf Ipopt-${IPOPT_VERSION}.tgz
+mv Ipopt-releases-${IPOPT_VERSION} Ipopt
+mkdir Ipopt/build
+rm "Ipopt-${IPOPT_VERSION}.tgz"
+
+# coin-or's third party ASL
+ASL_VERSION=2.0
+cd "${WAREDIR}" || exit
+wget https://github.com/coin-or-tools/ThirdParty-ASL/archive/stable/${ASL_VERSION}.zip
+unzip ${ASL_VERSION}.zip
+mv ThirdParty-ASL-stable-${ASL_VERSION} ThirdParty-ASL
+cd ThirdParty-ASL || exit
+./get.ASL
+./configure --prefix="${INSTDIR}"
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
+sudo make install
+rm "../${ASL_VERSION}.zip"
+
+# coin-or's third party Mumps
+MUMPS_VERSION=2.1
+cd "${WAREDIR}" || exit
+wget https://github.com/coin-or-tools/ThirdParty-Mumps/archive/stable/${MUMPS_VERSION}.zip
+unzip ${MUMPS_VERSION}.zip
+mv ThirdParty-Mumps-stable-${MUMPS_VERSION} ThirdParty-Mumps
+cd ThirdParty-Mumps || exit
+./get.Mumps
+./configure --prefix="${INSTDIR}"
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
+sudo make install
+rm "../${MUMPS_VERSION}.zip"
+
+# Make Ipopt
+cd "${WAREDIR}/Ipopt" || exit
+./configure --prefix="${INSTDIR}"
+make -j "$(grep -c "^processor" /proc/cpuinfo)"
+make test
+sudo make install
+
+# creates the necessary links and cache to the most recent shared libraries found
+# in the directories specified on the command line, in the file /etc/ld.so.conf,
+# and in the trusted directories (/lib and /usr/lib).
+sudo ldconfig
