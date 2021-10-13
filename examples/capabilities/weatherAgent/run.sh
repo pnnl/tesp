00d640eaee6a89bd892f21765ff3d46482a95ff3
d3j331 <mitch.pelton@pnnl.gov>
2021-10-13 15:12:18 -0400

    Updates to capabilties and scripts 4

--- examples/capabilities/weatherAgent/run.sh
+++ examples/capabilities/weatherAgent/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare -r TESP_SUPPORT=$TESP_INSTALL/share/support
-declare -r TMY3_PATH=$TESP_SUPPORT/weather
+
+TMY3_PATH=$TESPDIR/data/weather
 
 # generate the weather text file
 python3 -c "import tesp_support.api as tesp;tesp.weathercsv('${TMY3_PATH}/TX-Dallasfort_Worth_Intl_Ap.tmy3','weather.dat','2000-01-01 00:00:00','2000-01-07 00:00:00',2000)"
@@ -13,4 +13,3 @@ gridlabd -D TMY3_PATH=$TMY3_PATH weatherTester.glm > gridlabd_tmy3.log
 (export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && exec gridlabd -D USE_FNCS weatherTester.glm &> gridlabd_fncs.log &)
 (export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && export FNCS_CONFIG_FILE=tracer.yaml && exec fncs_tracer 1d tracer.out &> tracer.log &)
 (export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && export WEATHER_CONFIG=WeatherConfig.json && exec python3 -c "import tesp_support.api as tesp;tesp.startWeatherAgent('weather.dat')" &> weather.log &)
-
--- examples/capabilities/weatherAgent/runh.sh
+++ examples/capabilities/weatherAgent/runh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare -r TESP_SUPPORT=$TESP_INSTALL/share/support
-declare -r TMY3_PATH=$TESP_SUPPORT/weather
+
+TMY3_PATH=$TESPDIR/data/weather
 
 # generate the weather text file
 python3 -c "import tesp_support.api as tesp;tesp.weathercsv('${TMY3_PATH}/TX-Dallasfort_Worth_Intl_Ap.tmy3','weather.dat','2000-01-01 00:00:00','2000-01-07 00:00:00',2000)"
@@ -9,8 +9,7 @@ python3 -c "import tesp_support.api as tesp;tesp.weathercsv('${TMY3_PATH}/TX-Dal
 gridlabd -D TMY3_PATH=$TMY3_PATH weatherTester.glm > gridlabd_tmy3.log
 
 # run the text file through HELICS
-(exec helics_broker -f 3 --loglevel=4 --name=mainbroker &> broker_helics.log &)
+(exec helics_broker -f 3 --loglevel=warning --name=mainbroker &> broker_helics.log &)
 (exec gridlabd -D USE_HELICS weatherTester.glm &> gridlabd_helics.log &)
 (exec helics_recorder --input=helicsRecorder.txt --period 300s --stop 86401s &> recorder_helics.log &)
 (export WEATHER_CONFIG=HelicsWeatherConfig.json && exec python3 -c "import tesp_support.api as tesp;tesp.startWeatherAgent('weather.dat')" &> weather_helics.log &)
-
