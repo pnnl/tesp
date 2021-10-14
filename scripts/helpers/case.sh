438105b9351497ee1b18c469cbe44d976d9e6a7b
d3j331 <mitch.pelton@pnnl.gov>
2021-10-14 13:50:24 -0400

    Updates to capabilties and scripts 6

--- scripts/helpers/case.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ -z $1 ]]; then
-  exit
-fi
-python3 "${TESPDIR}/scripts/helpers/case.py" $1
--- scripts/helpers/plots.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ -z $1 ]]; then
-  exit
-fi
-python3 "${TESPDIR}/scripts/helpers/plots.py" $1
