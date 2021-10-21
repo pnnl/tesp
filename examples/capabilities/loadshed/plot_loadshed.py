959b5cb008a77f2e61f307496242cec09b3612b3
d3j331 <mitch.pelton@pnnl.gov>
2021-10-21 14:53:03 -0400

    Misc changes for examples

--- examples/capabilities/loadshed/plot_loadshed.py
+++ examples/capabilities/loadshed/plot_loadshed.py
@@ -47,7 +47,7 @@ meta_m = lst_m.pop('Metadata')
 times = list(map(int,list(lst_m.keys())))
 times.sort()
 print ("There are", len (times), "sample times beginning with", times[1] - times[0], "second intervals")
-hrs = np.array(times, dtype=np.float)
+hrs = np.array(times, dtype=float)
 denom = 3600.0
 hrs /= denom
 
@@ -68,7 +68,7 @@ for key, val in meta_m.items():
 	elif key == 'voltage12_min':
 		MTR_VOLT12_MIN_IDX = val['index']
 		MTR_VOLT12_MIN_UNITS = val['units']
-data_m = np.empty(shape=(len(mtr_keys), len(times), len(lst_m[t1][mtr_keys[0]])), dtype=np.float)
+data_m = np.empty(shape=(len(mtr_keys), len(times), len(lst_m[t1][mtr_keys[0]])), dtype=float)
 print ("\nConstructed", data_m.shape, "NumPy array for Meters")
 j = 0
 for key in mtr_keys:
@@ -93,7 +93,7 @@ for key, val in meta_s.items():
 	elif key == 'real_power_losses_avg':
 		SUB_LOSSES_IDX = val['index']
 		SUB_LOSSES_UNITS = val['units']
-data_s = np.empty(shape=(len(sub_keys), len(times), len(lst_s[t1][sub_keys[0]])), dtype=np.float)
+data_s = np.empty(shape=(len(sub_keys), len(times), len(lst_s[t1][sub_keys[0]])), dtype=float)
 print ("\nConstructed", data_s.shape, "NumPy array for Substations")
 j = 0
 for key in sub_keys:
