f73220110a8b6b05f76fcb79e48d0be1a3448459
d3j331 <mitch.pelton@pnnl.gov>
2021-10-26 18:48:24 -0400

    Update installing scripts....'

--- scripts/tesp_c.sh
+++ scripts/tesp_c.sh
@@ -10,16 +10,17 @@ echo
 # Install all pip libraries
 echo pip Installing Python Libraries
 #pip3 install wheel colorama glm seaborn matplotlib networkx==2.3 numpy pandas pulp pyutilib==5.8.0 pyomo==5.6.8 PYPOWER scikit-learn scipy tables h5py xlrd
-pip3 install wheel colorama glm seaborn matplotlib networkx numpy pandas pulp pyutilib==5.8.0 pyomo==5.6.8 PYPOWER scikit-learn scipy tables h5py xlrd 2>&1 pylib.log
+pip3 install wheel colorama glm seaborn matplotlib networkx numpy pandas pulp pyutilib==5.8.0 pyomo==5.6.8 PYPOWER scikit-learn scipy tables h5py xlrd > "${TESPDIR}/scripts/build/pylib.log" 2>&1
 
 #develop tesp api
 echo pip Installing TESP API
 cd "${TESPDIR}/src/tesp_support" || exit
-pip3 install -e . 2>&1 tespapi.log
+pip3 install -e . > "${TESPDIR}/scripts/build/tespapi.log" 2>&1
+
 #develop psst api
 echo pip Installing PSST
 cd "${REPODIR}/psst" || exit
-pip3 install -e . 2>&1 psst.log
+pip3 install -e . > "${TESPDIR}/scripts/build/psst.log" 2>&1
 
 #  pip3 install tesp_support --upgrade
 #  pip3 install psst --upgrade
@@ -28,23 +29,31 @@ cd "${TESPDIR}/scripts/build" || exit
 if [[ $1 == "develop" ]]; then
 
   echo Compiling and Installing FNCS
-  ./fncs_b.sh clean 2>&1 fncs.log
+  ./fncs_b.sh clean > fncs.log 2>&1
+
   echo Compiling and Installing FNCS for Java
-  ./fncs_java_b.sh clean 2>&1 fava_java.log
+  ./fncs_java_b.sh clean > fncs_java.log 2>&1
+
   echo Compiling and Installing HELICS
-  ./helics2_b.sh clean 2>&1 helics2.log
+  ./helics2_b.sh clean > helics2.log 2>&1
+
   echo Compiling and Installing KLU
-  ./klu_b.sh clean 2>&1 klu.log
+  ./klu_b.sh clean > klu.log 2>&1
+
   echo Compiling and Installing Gridlabd
-  ./gridlabd_b.sh clean 2>&1 gridlabd.log
+  ./gridlabd_b.sh clean > gridlabd.log 2>&1
+
   echo Compiling and Installing EnegryPlus
-  ./energyplus_b.sh clean 2>&1 energyplus.log
+  ./energyplus_b.sh clean > energyplus.log 2>&1
+
   echo Compiling and Installing EnergryPlus for Java
-  ./energyplusj_b.sh clean 2>&1 energyplusj.log
+  ./energyplusj_b.sh clean > energyplusj.log 2>&1
+
   echo Compiling and Installing NS-3
-  ./ns-3_b.sh clean 2>&1 ns3.log
+  ./ns-3_b.sh clean > ns3.log 2>&1
+
   echo Compiling and Installing Ipopt with ASL and Mumps
-  ./ipopt_b.sh clean 2>&1 ipopt.log
+  ./ipopt_b.sh clean > ipopt.log 2>&1
 else
 
   echo Installing TESP binaries
