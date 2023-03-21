0c45d567927699b1737a22121a6c178c85d3fe17
d3j331 <mitch.pelton@pnnl.gov>
2023-03-20 20:28:21 -0700

    fixes for SGIP1 scripts for weather

--- examples/analysis/sgip1/runSGIP1d.sh
+++ examples/analysis/sgip1/runSGIP1d.sh
@@ -4,11 +4,12 @@
 
 declare -r SCHED_PATH=$TESPDIR/data/schedules
 declare -r EPLUS_PATH=$TESPDIR/data/energyplus
+declare -r EPWFILE=$EPLUS_PATH/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw
 
 (export FNCS_BROKER="tcp://*:5570" && exec fncs_broker 6 &> broker1d.log &)
-(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w $EPLUS_PATH/USA_AZ_Tucson.Intl.AP.722740_TMY3.epw -d output -r $EPLUS_PATH/SchoolDualController_f.idf &> eplus1d.log &)
+(export FNCS_CONFIG_FILE=eplus.yaml && exec energyplus -w $EPWFILE -d output -r $EPLUS_PATH/SchoolDualController_f.idf &> eplus1d.log &)
 (export FNCS_CONFIG_FILE=eplus_agent.yaml && exec eplus_agent 2d 5m SchoolDualController eplus_SGIP1d_metrics.json &> eplus_agent1d.log &)
 (export FNCS_FATAL=YES && exec gridlabd -D SCHED_PATH=$SCHED_PATH -D USE_FNCS -D METRICS_FILE=SGIP1d_metrics.json SGIP1d.glm &> gridlabd1d.log &)
 (export FNCS_CONFIG_FILE=SGIP1b_substation.yaml && export FNCS_FATAL=YES && exec python3 -c "import tesp_support.substation as tesp;tesp.substation_loop('SGIP1d_agent_dict.json','SGIP1d')" &> substation1d.log &)
 (export FNCS_CONFIG_FILE=pypower.yaml && export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && exec python3 -c "import tesp_support.tso_PYPOWER_f as tesp;tesp.tso_pypower_loop_f('sgip1_pp.json','SGIP1d')" &> pypower1d.log &)
-(export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && export WEATHER_CONFIG=SGIP1b_weather.json && exec python3 -c "import tesp_support.weatherAgent as tesp;tesp.startWeatherAgent('weather.dat')" &> weather1d.log &)
+(export FNCS_FATAL=YES && export FNCS_LOG_STDOUT=yes && export WEATHER_CONFIG=SGIP1b_weather_f.json && exec python3 -c "import tesp_support.weatherAgent as tesp;tesp.startWeatherAgent('weather.dat')" &> weather1d.log &)
