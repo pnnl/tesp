9042c3dc21f46b302eb55f79538af217ba843fd6
d3j331 <mitch.pelton@pnnl.gov>
2023-05-10 13:26:07 -0700

    More documentation and api refactoring and msys2 install

--- doc/prereqs.sh
+++ doc/prereqs.sh
@@ -1,6 +1,6 @@
 pip3 install sphinx
 pip3 install sphinx-autobuild
-pip3 install sphinx-rtd-theme
+# pip3 install sphinx-rtd-theme
 pip3 install recommonmark
 pip3 install javasphinx
 pip3 install sphinxcontrib-bibtex
