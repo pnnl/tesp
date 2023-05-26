3a46edf65f8692d2d769e6fd09946a038686189f
d3j331 <mitch.pelton@pnnl.gov>
2023-05-26 09:39:56 -0700

    Changes for HELICS 3.4

--- examples/capabilities/loadshed/runhjava.sh
+++ examples/capabilities/loadshed/runhjava.sh
@@ -9,6 +9,6 @@ declare -r JAVAPATH=$INSTDIR/java
 javac -classpath ".:$JAVAPATH/helics.jar" helicshed.java
 
 (exec helics_broker -f 3 --loglevel=warning --name=mainbroker &> broker.log &)
-(exec helics_recorder --config-filehelicsRecorder.json --timedelta 1s --period 1s --stop 21600s &> tracer.log &)
+(exec helics_recorder --config-file helicsRecorder.json --timedelta 1s --period 1s --stop 21600s &> tracer.log &)
 (exec gridlabd -D WANT_HELICS_NO_NS3 loadshed.glm &> gridlabd.log &)
 (exec java -classpath ".:$JAVAPATH/helics.jar" -Djava.library.path="$JAVAPATH" helicshed &> java.log &)
