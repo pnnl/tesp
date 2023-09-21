d4d758fec4160a7c8f541dfef5d4a046514f5e32
trevorhardy <trevor.hardy@pnnl.gov>
2023-09-21 09:52:40 -0700

    Datastore example initial commit

--- examples/capabilities/datastore/te30_usestore.py
+++ examples/capabilities/datastore/te30_usestore.py
@@ -1,12 +1,38 @@
+# Copyright (C) 2023 Battelle Memorial Institute
+# file: te30_usestore.py
+""" 
+Takes existing datastore made by running the te30 example and processes the results. This
+demonstrates the prototype datastore capability in TESP.
+"""
+
 
 import json
-
 import tesp_support.api.store as fle
 
 
-def little_post():
-    pass
+def process_results():
+    """
+    Opens up datastore (.zip) and metadata (.json) to process results
+    
+    Assumes te30_store.zip and te30_store.json have been copied from ../te30 folder to
+    the same folder as this script (examples/capabilities/datastore).
+    """
 
+	# Load in metadata to see what contents are in the TE30 store
+	meta = json.loads('./te30_store.json')
+	
+	# List all the files in the store for inspection
+	for item in meta['store']:
+		print(item['name'])
+		
+	# Arbitrarily, let's look at the real power load at a specific billing meter
+	# This data is coming from GridLAB-D (based on the TE30 documentation)
+	
+	# metadata table has the name of each parameters and "value" is the unit. 
+	# Same order as what's in the index tables
+	
+	my_file.set_date_bycol(tables[1], columns[1])
+    my_file.set_date_bycol(tables[2], columns[1])
 
 if __name__ == "__main__":
-    little_post()
+    process_results()
