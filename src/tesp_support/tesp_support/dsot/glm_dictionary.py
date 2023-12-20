c24b2d1bdcae589619e7ace6f548c5e2e606d491
carolyngoodman1 <carolyn.goodman@pnnl.gov>
2023-12-20 14:30:24 -0800

    Added Moderate income

--- src/tesp_support/tesp_support/dsot/glm_dictionary.py
+++ src/tesp_support/tesp_support/dsot/glm_dictionary.py
@@ -309,6 +309,8 @@ def glm_dict(name_root, config=None, ercot=False):  # , te30=False):
                     name = lst[1].strip(';')
                     if 'Low' in name:
                         inc_level = 'Low'
+                    elif 'Moderate' in name:
+                        inc_level = 'Moderate'
                     elif 'Middle' in name:
                         inc_level = 'Middle'
                     elif 'Upper' in name:
