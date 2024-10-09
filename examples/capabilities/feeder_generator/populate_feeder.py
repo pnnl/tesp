1d6bdbf43b76610b64735d5465a468af59f1fbe4
jk1066 <jessica.kerby@pnnl.gov>
2024-10-09 15:44:27 -0700

    File cleanup

--- examples/capabilities/feeder_generator/populate_feeder.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (C) 2021-2023 Battelle Memorial Institute
-# file: populate_feeder.py
-
-import sys
-import tesp_support.original.tesp_case as tc
-
-tc.make_tesp_case(sys.argv[1])
